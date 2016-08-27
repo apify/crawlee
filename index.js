af6a0ee0b1774b7b89234ca404113228bcbfbead
Jan Curn <jan.curn@gmail.com>
2016-08-27 13:54:55 +0200

    Added index.js and package.json, updated LICENSE

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * Apifier SDK for JavaScript.
+ *
+ * This is a work in progress.
+ *
+ * Author: Jan Curn (jan@apifier.com)
+ * Copyright(c) 2016 Apifier. All rights reserved.
+ *
+ */
+"use strict";
+
+console.log("Apifier SDK for JavaScript");
+console.log("Copyright(c) 2016 Apifier. All rights reserved.");
+console.log("");
