0c90567476be492bce4d9b9ac51e23e94bbc772a
Jan Curn <jan.curn@gmail.com>
2017-04-19 13:08:42 +0200

    Bugfix in prepublishOnly event in package.json

--- build/index.js
+++ build/index.js
@@ -15,6 +15,6 @@ var _actor2 = _interopRequireDefault(_actor);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Apifier = {};
-_underscore2.default.extend(Apifier, Actorxx);
+_underscore2.default.extend(Apifier, _actor2.default);
 
 exports.default = Apifier;
