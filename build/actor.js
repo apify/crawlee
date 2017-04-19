454900655399e58e5e95b27b0395725219d3b886
Jan Curn <jan.curn@gmail.com>
2017-04-19 16:11:14 +0200

    Simplified Apifier.main() function logic

--- build/actor.js
+++ build/actor.js
@@ -4,48 +4,85 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _http = require('http');
-
-var _http2 = _interopRequireDefault(_http);
-
 var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = {
+/* global process */
 
-    main: function main(handler) {
-        if (!handler || typeof handler !== 'function') {
+var Actor = {
+    main: function main(userFunc) {
+        if (!userFunc || typeof userFunc !== 'function') {
             throw new Error('Handler function must be provided as a parameter');
         }
         var serverPort = parseInt(process.env.APIFIER_INTERNAL_PORT, 10);
         if (!(serverPort > 0 && serverPort < 65536)) {
-            throw new Error('APIFIER_INTERNAL_PORT environment variable must be a value from 1 to 65535.');
+            throw new Error('APIFIER_INTERNAL_PORT environment variable must have a value from 1 to 65535.');
         }
 
-        var watchFileName = process.env.APIFIER_WATCH_FILE;
+        var handler = function handler(req, res) {
+            var options = {
+                input: req.body
+            };
 
-        var wrappedHandler = function wrappedHandler(req, res) {
-            try {
-                handler(req, res);
-
-                // close response if not yet
-            } catch (e) {
-                console.log('Received message!!!');
+            Promise.resolve().then(function () {
+                return userFunc(options);
+            }).then(function () {
                 res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Hello World\n');
-            }
+            }).catch(function (err) {
+                console.log('User act failed: ' + err);
+                res.statusCode = 500;
+            }).then(function () {
+                res.end(function (err) {
+                    if (err) {
+                        console.log('Failed to send HTTP response: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('All good, finishing the act');
+                    process.exit(0);
+                });
+            }).catch(function (err) {
+                console.log('Something went terribly wrong: ' + err);
+                process.exit(2);
+            });
         };
 
-        var server = _http2.default.createServer(wrappedHandler);
+        var app = (0, _express2.default)();
 
-        server.listen(serverPort, function () {
+        // parse application/json
+        app.use(_bodyParser2.default.json());
+        app.use(handler);
+
+        // TODO: handle errors!
+        app.listen(serverPort, function () {
             console.log('Listening on port ' + serverPort);
-            if (watchFileName) _fs2.default.writeFileSync(watchFileName, '');
+            Actor.heyIAmReady();
         });
-    }
+    },
 
+
+    /**
+     * Notifies Apifier runtime that act is listening on port specified by the APIFIER_INTERNAL_PORT environment
+     * variable and is ready to receive a HTTP request with act input.
+     */
+    heyIAmReady: function heyIAmReady() {
+        var watchFileName = process.env.APIFIER_WATCH_FILE;
+        if (watchFileName) {
+            _fs2.default.writeFile(watchFileName, '', function (err) {
+                if (err) console.log('WARNING: Cannot write to watch file ' + watchFileName + ': ' + err);
+            });
+        }
+    }
 };
+
+exports.default = Actor;
--- src/actor.js
+++ src/actor.js
@@ -1,33 +1,61 @@
-import http from 'http';
 import fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+/* global process */
+
 
 const Actor = {
 
-    main(handler) {
-        if (!handler || typeof (handler) !== 'function') {
+    main(userFunc) {
+        if (!userFunc || typeof (userFunc) !== 'function') {
             throw new Error('Handler function must be provided as a parameter');
         }
         const serverPort = parseInt(process.env.APIFIER_INTERNAL_PORT, 10);
         if (!(serverPort > 0 && serverPort < 65536)) {
-            throw new Error('APIFIER_INTERNAL_PORT environment variable must be a value from 1 to 65535.');
+            throw new Error('APIFIER_INTERNAL_PORT environment variable must have a value from 1 to 65535.');
         }
 
-        const wrappedHandler = (req, res) => {
-            try {
-                handler(req, res);
+        const handler = (req, res) => {
+            const options = {
+                input: req.body,
+            };
 
-                // close response if not yet
-            } catch (e) {
-                console.log('Received message!!!');
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Hello World\n');
-            }
+            Promise.resolve()
+                .then(() => {
+                    return userFunc(options);
+                })
+                .then(() => {
+                    res.statusCode = 200;
+                })
+                .catch((err) => {
+                    console.log(`User act failed: ${err}`);
+                    res.statusCode = 500;
+                })
+                .then(() => {
+                    res.end((err) => {
+                        if (err) {
+                            console.log(`Failed to send HTTP response: ${err}`);
+                            process.exit(1);
+                        }
+                        console.log('All good, finishing the act');
+                        process.exit(0);
+                    });
+                })
+                .catch((err) => {
+                    console.log(`Something went terribly wrong: ${err}`);
+                    process.exit(2);
+                });
         };
 
-        const server = http.createServer(wrappedHandler);
+        const app = express();
 
-        server.listen(serverPort, () => {
+        // parse application/json
+        app.use(bodyParser.json());
+        app.use(handler);
+
+        // TODO: handle errors!
+        app.listen(serverPort, () => {
             console.log(`Listening on port ${serverPort}`);
             Actor.heyIAmReady();
         });
