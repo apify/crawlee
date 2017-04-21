4ae501ca137355f298f8d45fc53dc816a2b1e3a4
Jan Curn <jan.curn@gmail.com>
2017-04-21 19:52:22 +0100

    Cosmetic fix of comment

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const Apifier = {
 };
 
 
-// export this way so the we can import using:
+// export this way so that we can import using:
 // const Apifier = require('apifier');
 module.exports = Apifier;
