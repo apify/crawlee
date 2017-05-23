07833e023c191ffc181e2e2051f1ec8e08188223
Marek Trunkat <marek@trunkat.eu>
2017-05-23 17:13:57 +0200

    Increasing version

---
+++ publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+PACKAGE_VERSION=`node -pe "require('./package.json').version"`
+BRANCH=`git status | grep 'On branch' | cut -d ' ' -f 3`
+BRANCH_UP_TO_DATE=`git status | grep 'nothing to commit' | tr -s \n ' '`;
+
+if [ -z "$BRANCH_UP_TO_DATE" ]; then
+    printf "${RED}You have uncommited changes!${NC}\n"
+    exit 1
+fi
+
+if [ $BRANCH = "master" ]; then
+    NPM_TAG='latest'
+    GIT_TAG="v${PACKAGE_VERSION}"
+else
+    NPM_TAG='beta'
+    GIT_TAG="v${PACKAGE_VERSION}-beta"
+fi
+
+echo "Pushing to git ..."
+git push
+
+echo "Publishing version ${PACKAGE_VERSION} with tag \"${NPM_TAG}\" ..."
+npm publish --tag $NPM_TAG
+
+echo "Tagging git with ${GIT_TAG} ..."
+git tag ${GIT_TAG}
+git push origin ${GIT_TAG}
+
+echo "Done."
