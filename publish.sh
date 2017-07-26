843504f62b04c0c1e2d85c33ae8ab6d3b6e198ad
Jan Curn <jan.curn@gmail.com>
2017-07-26 17:45:30 +0200

    Updated publish.sh, bumped package.json version

--- publish.sh
+++ publish.sh
@@ -11,8 +11,8 @@ BRANCH=`git status | grep 'On branch' | cut -d ' ' -f 3`
 BRANCH_UP_TO_DATE=`git status | grep 'nothing to commit' | tr -s \n ' '`;
 GIT_TAG="v${PACKAGE_VERSION}"
 
-if [ -z "$BRANCH_UP_TO_DATE" ]; then
-    printf "${RED}You have uncommited changes!${NC}\n"
+if [ -z "${BRANCH_UP_TO_DATE}" ]; then
+    printf "${RED}You have uncommitted changes!${NC}\n"
     exit 1
 fi
 
@@ -20,17 +20,17 @@ echo "Pushing to git ..."
 git push
 
 # Master gets published as LATEST if that version doesn't exists yet and retagged as LATEST otherwise.
-if [ $BRANCH = "master" ]; then
-    if [ -z `npm view ${PACKAGE_NAME} versions | grep $PACKAGE_VERSION` ]; then
-        echo "Publishing version ${PACKAGE_VERSION} with tag \"latest\" ..."
-        RUNNING_FROM_SCRIPT=1 npm publish --tag latest
+if [ "${BRANCH}" = "master" ]; then
+    if [ -z `npm view ${PACKAGE_NAME} versions | grep ${PACKAGE_VERSION}` ]; then
+        printf "${RED}You can only publish to NPM from develop branch with beta tag!${NC}\n"
+        exit 1
     else
         echo "Tagging version ${PACKAGE_VERSION} with tag \"latest\" ..."
         RUNNING_FROM_SCRIPT=1 npm dist-tag add ${PACKAGE_NAME}@${PACKAGE_VERSION} latest
     fi
 
 # Develop branch gets published as BETA and we don't allow to override tag of existing version.
-elif [ $BRANCH = "develop" ]; then
+elif [ "${BRANCH}" = "develop" ]; then
     echo "Publishing version ${PACKAGE_VERSION} with tag \"beta\" ..."
     RUNNING_FROM_SCRIPT=1 npm publish --tag beta
 
