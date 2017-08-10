4459f487f59d7f57998ea4b3f1993fe7eaa9e326
Marek Trunkat <marek@trunkat.eu>
2017-08-10 10:21:14 +0200

    Implementing Apify.call()

--- src/actor.js
+++ src/actor.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import _ from 'underscore';
-import { ENV_VARS, EXIT_CODES } from './constants';
+import { checkParamOrThrow } from 'apify-client/build/utils';
+import { MAX_WAIT_FOR_FINISH_SECS } from 'apify-client/build/acts';
+import { ENV_VARS, EXIT_CODES, ACT_TASK_TERMINAL_STATUSES, DEFAULT_APIFY_CALL_WAIT_SECS } from './constants';
 import { getPromisePrototype, newPromise, nodeifyPromise, newClient } from './utils';
 
 /* global process, Buffer */
@@ -258,4 +260,69 @@ export const readyFreddy = () => {
     } else {
         console.log(`WARNING: ${ENV_VARS.WATCH_FILE} environment variable not specified, readyFreddy() has no effect.`);
     }
+};
+
+export const call = (actId, opts = {}) => {
+    const { acts, keyValueStores } = apifyClient;
+
+    // Common options.
+    const { token } = opts;
+    checkParamOrThrow(actId, 'actId', 'String');
+    checkParamOrThrow(token, 'token', 'Maybe String');
+    const defaultOpts = { actId };
+    if (token) defaultOpts.token = token;
+
+    // RunAct() options.
+    const { build, body, contentType } = opts;
+    checkParamOrThrow(build, 'build', 'Maybe String');
+    checkParamOrThrow(body, 'body', 'Maybe Buffer | String');
+    checkParamOrThrow(contentType, 'contentType', 'Maybe String');
+    const runActOpts = {};
+    if (contentType) runActOpts.contentType = contentType;
+    if (build) runActOpts.build = build;
+    if (body) runActOpts.build = body;
+
+    // GetAct() options.
+    const { timeoutSecs = DEFAULT_APIFY_CALL_WAIT_SECS, fetchOutput = true } = opts;
+    checkParamOrThrow(timeoutSecs, 'timeoutSecs', 'Number');
+    checkParamOrThrow(fetchOutput, 'fetchOutput', 'Boolean');
+    const timeoutMillis = timeoutSecs * 1000;
+    const timeoutAt = Date.now() + timeoutMillis;
+
+    // GetRecord() options.
+    const { raw, useRawBody } = opts;
+    checkParamOrThrow(raw, 'raw', 'Maybe Boolean');
+    checkParamOrThrow(useRawBody, 'useRawBody', 'Maybe Boolean');
+
+    // Adds run.output field to given run and returns it.
+    const addOutputToRun = (run) => {
+        const getRecordOpts = { key: 'OUTPUT', storeId: run.defaultKeyValueStoreId };
+        if (raw) getRecordOpts.raw = raw;
+        if (useRawBody) getRecordOpts.useRawBody = useRawBody;
+
+        return keyValueStores
+            .getRecord(getRecordOpts)
+            .then(output => Object.assign({}, run, { output }));
+    };
+
+    // Keeps requesting given run until it gets finished or timeout is reached.
+    const waitForRunToFinish = (run) => {
+        const waitForFinish = Math.min(MAX_WAIT_FOR_FINISH_SECS, Math.round((timeoutAt - Date.now()) / 1000));
+
+        // We are timing out ...
+        if (waitForFinish <= 0) return Promise.resolve(run);
+
+        return acts
+            .getRun(Object.assign({}, defaultOpts, { waitForFinish, runId: run.id }))
+            .then((updatedRun) => {
+                if (!_.contains(ACT_TASK_TERMINAL_STATUSES, updatedRun.status)) return waitForRunToFinish(updatedRun);
+                if (!fetchOutput) return run;
+
+                return addOutputToRun(updatedRun);
+            });
+    };
+
+    return acts
+        .runAct(Object.assign({}, defaultOpts, runActOpts))
+        .then(run => waitForRunToFinish(run));
 };
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,10 @@
 
 /**
+ * Default wait of Apify.call() method.
+ */
+export const DEFAULT_APIFY_CALL_WAIT_SECS = 180;
+
+/**
  * Exit codes for the act process.
  * The error codes must be in range 1-128, to avoid collision with signal exits
  * and to ensure Docker will handle them correctly!
@@ -27,8 +32,31 @@ export const ENV_VARS = {
     INTERNAL_PORT: 'APIFY_INTERNAL_PORT',
 };
 
-
 export const KEY_VALUE_STORE_KEYS = {
     INPUT: 'INPUT',
     OUTPUT: 'OUTPUT',
 };
+
+/**
+ * Dictionary of possible values for 'status' field of act2Builds or act2Runs collections.
+ */
+export const ACT_TASK_STATUSES = {
+    READY: 'READY', // started but not allocated to any worker yet
+    RUNNING: 'RUNNING', // running on worker
+    SUCCEEDED: 'SUCCEEDED', // finished and all good
+    FAILED: 'FAILED', // run or build failed
+    TIMING_OUT: 'TIMING-OUT',  // timing out now
+    TIMED_OUT: 'TIMED-OUT', // timed out
+    ABORTING: 'ABORTING', // being aborted by user
+    ABORTED: 'ABORTED', // aborted by user
+};
+
+/**
+ * An array of act task statuses that are final for the task.
+ */
+export const ACT_TASK_TERMINAL_STATUSES = [
+    ACT_TASK_STATUSES.SUCCEEDED,
+    ACT_TASK_STATUSES.FAILED,
+    ACT_TASK_STATUSES.TIMED_OUT,
+    ACT_TASK_STATUSES.ABORTED,
+];
