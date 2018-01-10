33dd746b18c118687f27c17ab713a059691ef26b
Jan Curn <jan.curn@gmail.com>
2018-01-10 19:21:05 +0100

    Updated default user agent

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'; // eslint-disable-line max-len
+export const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'; // eslint-disable-line max-len
 
 /**
  * Exit codes for the act process.
