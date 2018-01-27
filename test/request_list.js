449e9edef21f1bedf23a89393e14de24e5746af6
Marek Trunkat <marek@trunkat.eu>
2018-01-27 14:47:41 +0100

    Adding test for load from external request list and regex

--- test/request_list.js
+++ test/request_list.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import { expect } from 'chai';
+import request from 'request-promise';
+import sinon from 'sinon';
 import Apify from '../build/index';
 import { computeUniqueKey } from '../build/request';
 
@@ -49,6 +51,76 @@ describe('Apify.RequestList', () => {
         expect(requestList.fetchNextRequest().url).to.be.eql('https://example.com/7');
         expect(requestList.fetchNextRequest().url).to.be.eql('https://example.com/8');
         expect(requestList.isEmpty()).to.be.eql(true);
+    });
+
+    it('should correctly load list from hosted files in correct order', async () => {
+        const mock = sinon.mock(request);
+        const list1 = [
+            'https://example.com',
+            'https://google.com',
+            'https://wired.com',
+        ];
+        const list2 = [
+            'https://another.com',
+            'https://page.com',
+        ];
+
+        mock.expects('get')
+            .once()
+            .withArgs('http://example.com/list-1')
+            .returns(new Promise(resolve => setTimeout(resolve(list1.join('\n')), 100)));
+
+        mock.expects('get')
+            .once()
+            .withArgs('http://example.com/list-2')
+            .returns(Promise.resolve(list2.join('\n')), 0);
+
+        const requestList = new Apify.RequestList({
+            sources: [
+                { method: 'GET', requestsFromUrl: 'http://example.com/list-1' },
+                { method: 'POST', requestsFromUrl: 'http://example.com/list-2' },
+            ],
+        });
+
+        await requestList.loadSources();
+
+        expect(requestList.fetchNextRequest()).to.include({ method: 'GET', url: list1[0] });
+        expect(requestList.fetchNextRequest()).to.include({ method: 'GET', url: list1[1] });
+        expect(requestList.fetchNextRequest()).to.include({ method: 'GET', url: list1[2] });
+        expect(requestList.fetchNextRequest()).to.include({ method: 'POST', url: list2[0] });
+        expect(requestList.fetchNextRequest()).to.include({ method: 'POST', url: list2[1] });
+
+        mock.verify();
+        mock.restore();
+    });
+
+    it('should support regex parameter for hosted file list', async () => {
+        const mock = sinon.mock(request);
+        const listStr = 'kjnjkn"https://example.com/a/b/c?q=1#abc";,"http://google.com/a/b/c";dgg:dd';
+        const listArr = ['https://example.com/a/b/c?q=1#abc', 'http://google.com/a/b/c'];
+
+        mock.expects('get')
+            .once()
+            .withArgs('http://example.com/list-1')
+            .returns(Promise.resolve(listStr));
+
+        const requestList = new Apify.RequestList({
+            sources: [
+                {
+                    method: 'GET',
+                    requestsFromUrl: 'http://example.com/list-1',
+                    regex: '(http|https)://[\\w-]+(\\.[\\w-]+)+([\\w-.,@?^=%&:/~+#-]*[\\w@?^=%&;/~+#-])?',
+                },
+            ],
+        });
+
+        await requestList.loadSources();
+
+        expect(requestList.fetchNextRequest()).to.include({ method: 'GET', url: listArr[0] });
+        expect(requestList.fetchNextRequest()).to.include({ method: 'GET', url: listArr[1] });
+
+        mock.verify();
+        mock.restore();
     });
 
     it('should correctly handle reclaimed pages', async () => {
