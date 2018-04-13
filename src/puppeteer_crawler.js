d9d00072bc419152461acf6be89466dafa0adc6f
Marek Trunkat <marek@trunkat.eu>
2018-04-13 16:28:11 +0200

    Linting

--- src/puppeteer_crawler.js
+++ src/puppeteer_crawler.js
@@ -97,7 +97,7 @@ export default class PuppeteerCrawler {
     constructor(opts) {
         // For backwards compatibility, in the future we can remove this...
         if (!opts.retireInstanceAfterRequestCount && opts.abortInstanceAfterRequestCount) {
-            log.warning('PuppeteerCrawler: Parameter `abortInstanceAfterRequestCount` is deprecated!!! Use `retireInstanceAfterRequestCount` instead!');
+            log.warning('PuppeteerCrawler: Parameter `abortInstanceAfterRequestCount` is deprecated! Use `retireInstanceAfterRequestCount` instead!');
             opts.retireInstanceAfterRequestCount = opts.abortInstanceAfterRequestCount;
         }
 
