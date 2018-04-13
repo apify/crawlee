f76b84dfc887c41e421f26344d24fa42716ddb30
Marek Trunkat <marek@trunkat.eu>
2018-04-13 15:10:41 +0200

    Linting and fixing typo

--- src/errors.js
+++ src/errors.js
@@ -1,9 +1,9 @@
 export const APIFY_CALL_ERROR_NAME = 'APIFY_CALL_ERROR';
-export const APIFY_CALL_ERROR_MESSAGE = 'Apify.call() wasn\'t succeeded'
+export const APIFY_CALL_ERROR_MESSAGE = 'Apify.call() wasn\'t succeed';
 
 /**
  * @typedef {Object} ApifyCallError
- * @property {String} message=Apify.call()&nbsp;wasn't&nbsp;succeeded
+ * @property {String} message=Apify.call()&nbsp;wasn't&nbsp;succeed
  * @property {String} name=APIFY_CALL_ERROR
  * @property {Object} run Object of the failed run.
  */
