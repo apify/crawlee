6b1a71baf23b87d97e984e7b24cfdbdcc5cc1884
Marek Trunkat <marek@trunkat.eu>
2018-05-10 14:40:25 +0200

    Not ignoiring main process in BasicCrawler

--- src/puppeteer_crawler.js
+++ src/puppeteer_crawler.js
@@ -162,6 +162,7 @@ export default class PuppeteerCrawler {
             minConcurrency,
             minFreeMemoryRatio,
             isFinishedFunction,
+            ignoreMainProcess: true,
         });
     }
 
