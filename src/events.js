b1d14d96baa1881f3d28dc98c0bd3318e9397e4a
Jan Curn <jan.curn@gmail.com>
2018-09-12 18:10:14 +0200

    ACTOR_EVENTS_WS_URL log message changed from INFO to DEBUG

--- src/events.js
+++ src/events.js
@@ -119,7 +119,7 @@ export const initializeEvents = () => {
 
     // Locally there is no web socket to connect so just print a log message.
     if (!eventsWsUrl) {
-        log.info(`Apify.events: Environment variable ${ENV_VARS.ACTOR_EVENTS_WS_URL} is not set, no events from Apify platform will be emitted.`);
+        log.debug(`Apify.events: Environment variable ${ENV_VARS.ACTOR_EVENTS_WS_URL} is not set, no events from Apify platform will be emitted.`);
         return;
     }
 
