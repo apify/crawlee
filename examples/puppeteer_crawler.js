e53e5e2d6edbf086775f34da8e97e1bc232b0ffc
Jan Curn <jan.curn@gmail.com>
2018-10-30 17:31:00 +0100

    Fixed Puppeteer crawler example

--- examples/puppeteer_crawler.js
+++ examples/puppeteer_crawler.js
@@ -14,7 +14,7 @@ Apify.main(async () => {
     // Create and initialize an instance of the RequestList class that contains the start URL.
     const requestList = new Apify.RequestList({
         sources: [
-            { url: 'http://www.ycombinator.com/' },
+            { url: 'https://news.ycombinator.com/' },
         ],
     });
     await requestList.initialize();
@@ -32,7 +32,10 @@ Apify.main(async () => {
 
         // Here you can set options that are passed to the Apify.launchPuppeteer() function.
         // For example, you can set "slowMo" to slow down Puppeteer operations to simplify debugging
-        launchPuppeteerOptions: { slowMo: 2000 },
+        launchPuppeteerOptions: { slowMo: 500 },
+
+        // Stop crawling after several pages
+        maxRequestsPerCrawl: 10,
 
         // This function will be called for each URL to crawl.
         // Here you can write the Puppeteer scripts you are familiar with,
