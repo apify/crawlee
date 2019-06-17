c6f7d12741a95b1c35851a47979a5caade6cf353
Ondra Urban <ondra@apify.com>
2019-06-17 16:49:19 +0200

    Add validation to release script

--- tools/release.js
+++ tools/release.js
@@ -6,14 +6,26 @@ const semver = require('semver'); // eslint-disable-line import/no-extraneous-de
 const VERSION = require('../package.json').version;
 
 log('Checking if current branch is master branch.');
-const currentBranch = execSync('git branch | grep "*"', { encoding: 'utf8' }).substr(2);
-if (currentBranch !== 'master') {
-    log('Release can only be triggered from the master branch.');
+const gitStatus = execSync('git status --porcelain --branch', { encoding: 'utf8' });
+if (!gitStatus.startsWith('## feature/')) { // TODO for testing, dont forget to replace
+    log('Release can only be triggered from the master branch. Please checkout master.');
+    process.exit(1);
+}
+
+log('Checking if all changes are committed.');
+const statusLines = gitStatus.split('\n');
+if (statusLines.length > 1 && statusLines[1].length) {
+    log('You have uncommitted changes. Please commit them and run the script again.');
     process.exit(1);
 }
 
 log('Checking if branch is up to date.');
-const upToDate = execSync('git branch | grep "*"', { encoding: 'utf8' }).substr(2);
+if (/\[ahead|behind \d+\]/.test(gitStatus)) {
+    log('Your local copy of master branch is not up to date with the remote. Please push / pull changes.');
+    process.exit(1);
+}
+
+
 
 log('Fetching version of published beta.');
 const betaVersion = fetchPackageJsonPropertyForTag('version', 'beta');
