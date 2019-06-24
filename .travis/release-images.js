ed61d2bed2372dc95058aef8b0ba914f3a52a824
Ondra Urban <ondra@apify.com>
2019-06-24 17:19:17 +0200

    Re-encrypt NPM token for Travis

--- .travis/release-images.js
+++ .travis/release-images.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { execSync } = require('child_process');
 
 const TAG = process.env.TRAVIS_TAG;
@@ -5,12 +6,12 @@ const BRANCH = process.env.TRAVIS_BRANCH;
 
 if (TAG && /^v\d+\.\d+\.\d+$/.test(TAG)) {
     // Latest release
-    console.log('release-images: Triggering release of latest images');
-    execSync('node ./release-images-latest.js --dry-run');
+    console.log('release-images: Triggering release of latest images.');
+    execSync(`node ${path.join(__dirname, 'release-images-latest.js')} --dry-run'`);
 } else if (BRANCH && /^master$/.test(BRANCH)) {
     // Beta release
-    console.log('release-images: Triggering release of beta images');
-    execSync('node ./release-images-beta.js --dry-run');
+    console.log('release-images: Triggering release of beta images.');
+    execSync(`node ${path.join(__dirname, 'release-images-beta.js')} --dry-run`);
 } else {
     console.log('release-images: Build is not a release build. Skipping.');
     process.exit(0);
