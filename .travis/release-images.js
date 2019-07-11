060265de6b0b2e51a0f6f8a77292e0a249dbbaf3
Ondra Urban <ondra@apify.com>
2019-07-11 13:11:11 +0200

    Update travis configuration

--- .travis/release-images.js
+++ .travis/release-images.js
@@ -7,7 +7,7 @@ const BRANCH = process.env.TRAVIS_BRANCH;
 if (TAG && /^v\d+\.\d+\.\d+$/.test(TAG)) {
     // Latest release
     console.log('release-images: Triggering release of latest images.');
-    execSync(`node ${path.join(__dirname, 'release-images-latest.js')} --dry-run'`, { stdio: 'inherit'});
+    execSync(`node ${path.join(__dirname, 'release-images-latest.js')}`, { stdio: 'inherit'});
 } else if (BRANCH && /^master$/.test(BRANCH)) {
     // Beta release
     console.log('release-images: Triggering release of beta images.');
