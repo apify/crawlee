905fffcfc5dfdaed45e9e6930f57047c8d2b8060
Ondra Urban <ondra@apify.com>
2020-03-04 17:32:03 +0100

    Bump version, update CI, fix engines [skip ci]

--- types/puppeteer_request_interception.d.ts
+++
@@ -1,5 +0,0 @@
-export function addInterceptRequestHandler(page: Page, handler: InterceptHandler): Promise<void>;
-export function removeInterceptRequestHandler(page: Page, handler: InterceptHandler): Promise<void>;
-export type InterceptHandler = (request: PuppeteerRequest) => any;
-import { Page } from "puppeteer";
-import { Request as PuppeteerRequest } from "puppeteer";
