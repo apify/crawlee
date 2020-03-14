986f4d92a3c2c802a40ffca1ec837a91134b47e4
Jan Curn <jan.curn@gmail.com>
2020-03-14 13:29:55 +0100

    Improved Apify.addWebhook() docs

--- src/actor.js
+++ src/actor.js
@@ -735,9 +735,8 @@ export const getApifyProxyUrl = (options = {}) => {
  *   It uses JSON syntax, extended with a double curly braces syntax for injecting variables `{{variable}}`.
  *   Those variables are resolved at the time of the webhook's dispatch, and a list of available variables with their descriptions
  *   is available in the [Apify webhook documentation](https://docs.apify.com/webhooks).
- *
- *   When omitted, the default payload template will be used.
- *   [See the docs for the default payload template](https://docs.apify.com/webhooks).
+ *   If `payloadTemplate` is omitted, the default payload template is used
+ *   ([view docs](https://docs.apify.com/webhooks/actions#payload-template)).
  * @param {string} [options.idempotencyKey]
  *   Idempotency key enables you to ensure that a webhook will not be added multiple times in case of
  *   an actor restart or other situation that would cause the `addWebhook()` function to be called again.
