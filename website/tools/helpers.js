a3284397815cd9c6d07e7e465867a74793706048
Ondra Urban <ondra@apify.com>
2020-03-17 09:06:00 +0100

    Remove leading dots from nested param lists

--- website/tools/helpers.js
+++ website/tools/helpers.js
@@ -32,3 +32,23 @@ exports.class = (options) => {
     }
     return result ? options.fn(result) : 'ERROR, Cannot find class.';
 };
+
+exports.params = function params(options) {
+    if (this.params) {
+        const list = this.params.map((param) => {
+            const nameSplit = param.name.split('.');
+            let name = nameSplit[nameSplit.length - 1];
+            if (param.variable) name = `...${name}`;
+            if (param.optional) name = `[${name}]`;
+            return {
+                indent: '    '.repeat(nameSplit.length - 1),
+                name,
+                type: param.type,
+                optional: param.optional,
+                defaultvalue: param.defaultvalue,
+                description: param.description,
+            };
+        });
+        return options.fn(list);
+    }
+};
