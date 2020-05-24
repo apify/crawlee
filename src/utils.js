d0901a437b647d879e4d414c8306268500785ce3
Charlie Orford <charlie.orford@daltix.com>
2020-05-24 21:14:22 +0000

    Docstring fix

--- src/utils.js
+++ src/utils.js
@@ -672,7 +672,8 @@ export const printOutdatedSdkWarning = () => {
  * Gets parsed content type from response object
  * @param {IncomingMessage} response - HTTP response object
  * @return {{ type: string, charset: string }}
- * @ignore
+ * @memberOf utils
+ * @function
  */
 export const parseContentTypeFromResponse = (response) => {
     checkParamOrThrow(response, 'response', 'Object');
