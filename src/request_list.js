15b0c955ad36fe6b49aef5242ca6c1599dbb0050
Lukáš Křivka <lukaskrivka@gmail.com>
2020-05-31 15:15:10 +0200

    Be more explicit about state in RequestList docs (#708)

--- src/request_list.js
+++ src/request_list.js
@@ -174,7 +174,7 @@ const CONTENT_TYPE_BINARY = 'application/octet-stream';
  * **Basic usage:**
  * ```javascript
  * // Use a helper function to simplify request list initialization.
- * // State and sources are automatically persisted.
+ * // State and sources are automatically persisted. This is a preferred usage.
  * const requestList = await Apify.openRequestList('my-request-list', [
  *     'http://www.example.com/page-1',
  *     { url: 'http://www.example.com/page-2', method: 'POST', userData: { foo: 'bar' }},
@@ -196,7 +196,8 @@ const CONTENT_TYPE_BINARY = 'application/octet-stream';
  *         { requestsFromUrl: 'http://www.example.com/my-url-list.txt', userData: { isFromUrl: true } },
  *     ],
  *
- *     // Persist only state in cases where the original sources are immutable to improve performance.
+ *     // Persist the state to avoid re-crawling which can lead to data duplications.
+ *     // Keep in mind that the sources have to be immutable or this will throw an error.
  *     persistStateKey: 'my-state',
  * });
  *
