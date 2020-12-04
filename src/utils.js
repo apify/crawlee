0c15b37a0b23b6f8c5ae49d7ab7d52a7ef54d1f6
Ondra Urban <ondra@apify.com>
2020-12-04 19:50:22 +0100

    Fix Chrome path on Windows

--- src/utils.js
+++ src/utils.js
@@ -360,7 +360,7 @@ export const getFirstKey = (dict) => {
 export const getTypicalChromeExecutablePath = () => {
     switch (os.platform()) {
         case 'darwin': return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-        case 'win32': return 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+        case 'win32': return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
         default: return 'google-chrome';
     }
 };
