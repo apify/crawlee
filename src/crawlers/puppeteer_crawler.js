06477832741a51ecd4f0bb16e7822c638ec767fb
Petr Patek <petrpatekml@gmail.com>
2021-01-08 14:43:48 +0100

    refactored puppeteer-crawler

--- src/crawlers/puppeteer_crawler.js
+++ src/crawlers/puppeteer_crawler.js
@@ -148,21 +148,21 @@ import applyStealthToBrowser from '../stealth/stealth';
  * which downloads the pages using raw HTTP requests and is about 10x faster.
  *
  * The source URLs are represented using {@link Request} objects that are fed from
- * {@link RequestList} or {@link RequestQueue} instances provided by the {@link BasicCrawlerOptions.requestList}
- * or {@link BasicCrawlerOptions.requestQueue} constructor options, respectively.
+ * {@link RequestList} or {@link RequestQueue} instances provided by the {@link PuppeteerCrawlerOptions.requestList}
+ * or {@link PuppeteerCrawlerOptions.requestQueue} constructor options, respectively.
  *
- * If both {@link BasicCrawlerOptions.requestList} and {@link BasicCrawlerOptions.requestQueue} are used,
+ * If both {@link PuppeteerCrawlerOptions.requestList} and {@link PuppeteerCrawlerOptions.requestQueue} are used,
  * the instance first processes URLs from the {@link RequestList} and automatically enqueues all of them
  * to {@link RequestQueue} before it starts their processing. This ensures that a single URL is not crawled multiple times.
  *
  * The crawler finishes when there are no more {@link Request} objects to crawl.
  *
  * `PuppeteerCrawler` opens a new Chrome page (i.e. tab) for each {@link Request} object to crawl
- * and then calls the function provided by user as the {@link BrowserCrawlerOptions.handlePageFunction} option.
+ * and then calls the function provided by user as the {@link PuppeteerCrawlerOptions.handlePageFunction} option.
  *
  * New pages are only opened when there is enough free CPU and memory available,
  * using the functionality provided by the {@link AutoscaledPool} class.
- * All {@link AutoscaledPool} configuration options can be passed to the {@link BasicCrawlerOptions.autoscaledPoolOptions}
+ * All {@link AutoscaledPool} configuration options can be passed to the {@link PuppeteerCrawlerOptions.autoscaledPoolOptions}
  * parameter of the `PuppeteerCrawler` constructor. For user convenience, the `minConcurrency` and `maxConcurrency`
  * {@link AutoscaledPoolOptions} are available directly in the `PuppeteerCrawler` constructor.
  *
@@ -237,8 +237,7 @@ class PuppeteerCrawler extends BrowserCrawler {
         ow(options, 'PuppeteerCrawlerOptions', ow.object.exactShape(PuppeteerCrawler.optionsShape));
 
         const {
-            puppeteerModule, // eslint-disable-line
-            launchContext = {},
+            launchContext = {}, // @TODO: should not launcher be inside launchContext
             gotoTimeoutSecs,
             navigationTimeoutSecs,
             browserPoolOptions = {},
@@ -250,16 +249,17 @@ class PuppeteerCrawler extends BrowserCrawler {
             stealth,
             stealthOptions,
             proxyUrl,
+            launcher,
         } = launchContext;
 
         if (proxyUrl && proxyConfiguration) {
             throw new Error('It is not possible to combine "options.proxyConfiguration" together with '
-                + 'custom "proxyUrl" option from "options.launchPuppeteerOptions".');
+                + 'custom "proxyUrl" option from "options.launchContext".');
         }
 
         browserPoolOptions.browserPlugins = [
             new PuppeteerPlugin(
-                getPuppeteerOrThrow(puppeteerModule),
+                getPuppeteerOrThrow(launcher),
                 {
                     proxyUrl,
                     launchOptions: apifyOptionsToLaunchOptions(launchContext),
@@ -286,7 +286,7 @@ class PuppeteerCrawler extends BrowserCrawler {
         }
 
         if (proxyUrl) {
-            this.log.deprecated('options.launchPuppeteerOptions.proxyUrl is deprecated use the options.proxyConfiguration instead');
+            this.log.deprecated('options.launchContext.proxyUrl is deprecated use the options.proxyConfiguration instead');
         }
 
         if (stealth) {
@@ -299,7 +299,6 @@ class PuppeteerCrawler extends BrowserCrawler {
 
         this.navigationTimeoutMillis = (navigationTimeoutSecs || gotoTimeoutSecs) * 1000;
         this.launchContext = launchContext;
-        this.puppeteerModule = puppeteerModule;
 
         this.defaultGotoOptions = {
             timeout: this.navigationTimeoutMillis,
