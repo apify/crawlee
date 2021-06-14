80de678e41b232b8fe703f7b2fe85f64cbfeb0c7
Ondra Urban <ondra@apify.com>
2021-06-14 18:57:24 +0200

    chore: remove silent flag from jest to see logging for flaky tests

--- test/request.test.js
+++ test/request.test.js
@@ -120,7 +120,9 @@ describe('Apify.Request', () => {
         expect(() => new Apify.Request({ url: 'http://example.com', payload: 'foo', method: 'POST' })).not.toThrowError();
     });
 
-    test('should have acceptable request creation time', () => {
+    // TODO would be nice to have a test like this, but it's flaky in CI so I'm disabling it.
+    // Keeping it here to run it manually once in a while.
+    test.skip('should have acceptable request creation time', () => {
         const requests = [];
         const start = Date.now();
         for (let i = 0; i < 1000; i++) requests.push(new Apify.Request({ url: `https://example.com/${i}` }));
