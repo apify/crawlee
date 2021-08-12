e0ae762a7e40b278a97a8c1fbb1196efe8e76be5
Ondra Urban <23726914+mnmkng@users.noreply.github.com>
2021-08-12 09:22:34 +0200

    fix: cheerio issues with HTML serialization (#1113)
    * fix: cheerio issues with HTML serialization
    
    * fix: lint build issue
    
    * chore: update node setup in workflows
    
    * fix: TS test

--- test/crawlers/cheerio_crawler.test.js
+++ test/crawlers/cheerio_crawler.test.js
@@ -31,6 +31,52 @@ const responseSamples = {
         + '</item>\n'
         + '</items>',
     image: fs.readFileSync(path.join(__dirname, 'data/apify.png')),
+    html: '<!doctype html>\n'
+        + '<html>\n'
+        + '<head>\n'
+        + '    <title>Example Domain</title>\n'
+        + '\n'
+        + '    <meta charset="utf-8">\n'
+        + '    <meta http-equiv="Content-type" content="text/html; charset=utf-8">\n'
+        + '    <meta name="viewport" content="width=device-width, initial-scale=1">\n'
+        + '    <style type="text/css">\n'
+        + '    body {\n'
+        + '        background-color: #f0f0f2;\n'
+        + '        margin: 0;\n'
+        + '        padding: 0;\n'
+        + '        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n' // eslint-disable-line max-len
+        + '        \n'
+        + '    }\n'
+        + '    div {\n'
+        + '        width: 600px;\n'
+        + '        margin: 5em auto;\n'
+        + '        padding: 2em;\n'
+        + '        background-color: #fdfdff;\n'
+        + '        border-radius: 0.5em;\n'
+        + '        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n'
+        + '    }\n'
+        + '    a:link, a:visited {\n'
+        + '        color: #38488f;\n'
+        + '        text-decoration: none;\n'
+        + '    }\n'
+        + '    @media (max-width: 700px) {\n'
+        + '        div {\n'
+        + '            margin: 0 auto;\n'
+        + '            width: auto;\n'
+        + '        }\n'
+        + '    }\n'
+        + '    </style>    \n'
+        + '</head>\n'
+        + '\n'
+        + '<body>\n'
+        + '<div>\n'
+        + '    <h1>Example Domain</h1>\n'
+        + '    <p>This domain is for use in illustrative examples in documents. You may use this\n'
+        + '    domain in literature without prior coordination or asking for permission.</p>\n'
+        + '    <p><a href="https://www.iana.org/domains/example">More information...</a></p>\n'
+        + '</div>\n'
+        + '</body>\n'
+        + '</html>\n',
 };
 
 const app = express();
@@ -62,6 +108,10 @@ app.post('/jsonError', (req, res) => {
 
 app.get('/mirror', (req, res) => {
     res.send('<html><head><title>Title</title></head><body>DATA</body></html>');
+});
+
+app.get('/html-type', (req, res) => {
+    res.type('html').send(responseSamples.html);
 });
 
 app.get('/json-type', (req, res) => {
@@ -246,6 +296,24 @@ describe('CheerioCrawler', () => {
             handlePageFunction: async ({ contentType }) => {
                 const { type } = contentType;
                 expect(type).toEqual('application/json');
+            },
+        });
+
+        await cheerioCrawler.run();
+    });
+
+    test('should serialize body and html', async () => {
+        expect.assertions(2);
+        const sources = [`http://${HOST}:${port}/html-type`];
+        const requestList = await Apify.openRequestList(null, sources);
+
+        const cheerioCrawler = new Apify.CheerioCrawler({
+            requestList,
+            maxRequestRetries: 0,
+            maxConcurrency: 1,
+            handlePageFunction: async ({ $, body }) => {
+                expect(body).toBe(responseSamples.html);
+                expect($.html()).toBe(body);
             },
         });
 
