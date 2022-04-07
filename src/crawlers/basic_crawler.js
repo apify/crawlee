4f54355f8574bf727e63140a707d0539f97748f1
Martin Adámek <banan23@gmail.com>
2022-04-07 13:13:07 +0200

    chore: use `handleRequestTimeoutMillis * 2` when computing internal timeout value
    Related: #1324

--- src/crawlers/basic_crawler.js
+++ src/crawlers/basic_crawler.js
@@ -264,7 +264,7 @@ export class BasicCrawler {
         this.handleRequestTimeoutMillis = handleRequestTimeoutSecs * 1000;
         const tryEnv = (val) => (val == null ? val : +val);
         // allow at least 5min for internal timeouts
-        this.internalTimeoutMillis = tryEnv(process.env.APIFY_INTERNAL_TIMEOUT) ?? Math.max(this.handleRequestTimeoutMillis, 300e3);
+        this.internalTimeoutMillis = tryEnv(process.env.APIFY_INTERNAL_TIMEOUT) ?? Math.max(this.handleRequestTimeoutMillis * 2, 300e3);
         // override the default internal timeout of request queue to respect `handleRequestTimeoutMillis`
         if (this.requestQueue) {
             this.requestQueue.internalTimeoutMillis = this.internalTimeoutMillis;
