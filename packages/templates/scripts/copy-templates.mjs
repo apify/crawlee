658b431e05bbac8d14b0610924e119809f399b0a
Martin Adámek <banan23@gmail.com>
2022-07-18 15:32:47 +0200

    fix: rename default storage folder to just `storage`
    Given we use the memory storage also in the Actor SDK, we don't want to have a crawlee specific
    name for the default storage folder, as it feels confusing to have `crawlee_storage` folder when
    you don't interact with `crawlee` anyhow.
    
    This change should not be breaking, if a folder called `crawlee_storage` exists, we still use it.
    If not, we use just `storage`.

--- packages/templates/scripts/copy-templates.mjs
+++ packages/templates/scripts/copy-templates.mjs
@@ -6,7 +6,7 @@ const templates = await readdir('./templates');
 await copy('./manifest.json', './dist/manifest.json', { override: true });
 console.info(`Successfully copied 'manifest.json' to dist`);
 
-const ignoreFolders = ['node_modules', 'dist', 'crawlee_storage', 'apify_storage', 'package-lock.json'];
+const ignoreFolders = ['node_modules', 'dist', 'crawlee_storage', 'storage', 'apify_storage', 'package-lock.json'];
 
 for (const tpl of templates) {
     console.info(tpl);
