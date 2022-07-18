be07b6c649e3e4652ff99a72f2242acfe3abe06a
Martin Adámek <banan23@gmail.com>
2022-07-18 12:49:13 +0200

    docs: enable algolia

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -249,15 +249,14 @@ module.exports = {
                 height: '60px',
             },
         },
-        // disabled as we don't have docsearch approved for crawlee.dev yet
-        // algolia: {
-        //     appId: 'UXG5NIR52R',
-        //     apiKey: '83302bb4196d8377aa5b3526c6d904fb', // search only (public) API key
-        //     indexName: 'apify_sdk',
-        //     algoliaOptions: {
-        //         facetFilters: ['version:VERSION'],
-        //     },
-        // },
+        algolia: {
+            appId: '5JC94MPMLY',
+            apiKey: '267679200b833c2ca1255ab276731869', // search only (public) API key
+            indexName: 'crawlee',
+            algoliaOptions: {
+                facetFilters: ['version:VERSION'],
+            },
+        },
         gaGtag: {
             trackingID: 'UA-67003981-4',
         },
