bd5ca9478fcb0946149bc0b919c1451e156f55b4
Martin Adámek <banan23@gmail.com>
2022-07-22 16:55:48 +0200

    test: lets check this again

--- test/e2e/cheerio-default-ts/actor/main.ts
+++ test/e2e/cheerio-default-ts/actor/main.ts
@@ -1,4 +1,4 @@
-import { ApifyClient } from 'apify-client';
+import { Actor } from 'apify';
 import { Configuration, CheerioCrawler, Dataset } from '@crawlee/cheerio';
 import { ApifyStorageLocal } from '@apify/storage-local';
 
@@ -6,13 +6,7 @@ const config = Configuration.getGlobalConfig();
 config.set('availableMemoryRatio', 1);
 
 if (process.env.STORAGE_IMPLEMENTATION === 'PLATFORM') {
-    config.set('defaultDatasetId', process.env.APIFY_DEFAULT_DATASET_ID);
-    config.set('defaultKeyValueStoreId', process.env.APIFY_DEFAULT_KEY_VALUE_STORE_ID);
-    config.set('defaultRequestQueueId', process.env.APIFY_DEFAULT_REQUEST_QUEUE_ID);
-    config.useStorageClient(new ApifyClient({
-        baseUrl: process.env.APIFY_API_BASE_URL,
-        token: process.env.APIFY_TOKEN,
-    }));
+    await Actor.init();
 } else if (process.env.STORAGE_IMPLEMENTATION === 'LOCAL') {
     config.useStorageClient(new ApifyStorageLocal());
 }
