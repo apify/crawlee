b5784dcd5ede4b2174d8c864bb7e3034eff5a0cf
Martin Adámek <banan23@gmail.com>
2022-07-27 15:57:04 +0200

    docs: use GTM instead of gtag.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -100,11 +100,17 @@ module.exports = {
                         from: '/docs',
                         to: '/docs/quick-start',
                     },
-                //     {
-                //         from: '/docs/next',
-                //         to: '/docs/next/quick-start',
-                //     },
+                    // {
+                    //     from: '/docs/next',
+                    //     to: '/docs/next/quick-start',
+                    // },
                 ],
+            },
+        ],
+        [
+            require.resolve('docusaurus-gtm-plugin'),
+            {
+                id: 'GTM-TKBX678',
             },
         ],
     ],
@@ -275,9 +281,6 @@ module.exports = {
             algoliaOptions: {
                 facetFilters: ['version:VERSION'],
             },
-        },
-        gaGtag: {
-            trackingID: 'UA-67003981-4',
         },
     }),
 };
