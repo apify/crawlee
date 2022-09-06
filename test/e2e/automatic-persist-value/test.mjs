7c9b6e775b1c222225f3faa100cc278b7a30b88f
Martin Adámek <banan23@gmail.com>
2022-09-06 11:25:16 +0200

    test: add missing awaits on expect()

--- test/e2e/automatic-persist-value/test.mjs
+++ test/e2e/automatic-persist-value/test.mjs
@@ -5,13 +5,13 @@ await initialize(testActorDirname);
 
 const { keyValueStoreItems } = await runActor(testActorDirname);
 
-expect(keyValueStoreItems.length === 1, 'Key-value store automatically saved the value expected to be auto-saved');
+await expect(keyValueStoreItems.length === 1, 'Key-value store automatically saved the value expected to be auto-saved');
 
 const [{ name, raw }] = keyValueStoreItems;
 
-expect(name === 'crawlee', 'Key-value store auto-saved value is named "crawlee"');
+await expect(name === 'crawlee', 'Key-value store auto-saved value is named "crawlee"');
 
 const parsed = JSON.parse(raw.toString());
 
-expect(typeof parsed === 'object' && parsed !== null, 'Key-value store auto-save value is a non-nullable object');
-expect(parsed.crawlee === 'awesome!', 'Key-value store auto-save value has a property "crawlee" that is set to "awesome!"');
+await expect(typeof parsed === 'object' && parsed !== null, 'Key-value store auto-save value is a non-nullable object');
+await expect(parsed.crawlee === 'awesome!', 'Key-value store auto-save value has a property "crawlee" that is set to "awesome!"');
