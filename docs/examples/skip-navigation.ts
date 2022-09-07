3fc1951d79abba8c75c2facd0c9c52d11d6ed9aa
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2022-09-07 15:47:30 +0200

    docs: fix `skip-navigation` example (#1529)

--- docs/examples/skip-navigation.ts
+++ docs/examples/skip-navigation.ts
@@ -8,10 +8,10 @@ const crawler = new PlaywrightCrawler({
         // The request should have the navigation skipped
         if (request.skipNavigation) {
             // Request the image and get its buffer back
-            const imageBuffer = await sendRequest({ responseType: 'buffer' });
+            const imageResponse = await sendRequest({ responseType: 'buffer' });
 
             // Save the image in the key-value store
-            await imageStore.setValue(`${request.userData.key}.png`, imageBuffer);
+            await imageStore.setValue(`${request.userData.key}.png`, imageResponse.body);
 
             // Prevent executing the rest of the code as we do not need it
             return;
