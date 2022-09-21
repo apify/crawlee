852ca60f8f8399c0f0638fcccb3d21c5c544384a
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-09-21 12:48:41 +0200

    Update packages/utils/src/internals/open_graph_parser.ts
    Co-authored-by: Martin Adámek <banan23@gmail.com>

--- packages/utils/src/internals/open_graph_parser.ts
+++ packages/utils/src/internals/open_graph_parser.ts
@@ -386,7 +386,7 @@ const parseOpenGraphProperty = (property: OpenGraphProperty, $: CheerioAPI): str
  * Easily parse all OpenGraph properties from a page with just a `CheerioAPI` object.
  *
  * @param $ A `CheerioAPI` object
- * @param additionalProperties Any potential additional `OpenGraphProperty` items you'd like to be scraped. Currently existing properties are kept up to date.
+ * @param additionalProperties Any potential additional {@apilink OpenGraphProperty} items you'd like to be scraped. Currently existing properties are kept up to date.
  * @returns Scraped OpenGraph properties as an object.
  */
 export function parseOpenGraph($: CheerioAPI, additionalProperties?: OpenGraphProperty[]) {
