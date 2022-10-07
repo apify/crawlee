385ea1b13678aa0e12e6422eb49cc00d8e8fb11a
Jindřich Bär <jindrichbar@gmail.com>
2022-10-07 11:59:08 +0200

    docs: fix circular Examples category

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -147,7 +147,7 @@ module.exports = {
                 },
                 {
                     type: 'doc',
-                    docId: 'examples/examples',
+                    docId: '/examples',
                     label: 'Examples',
                     position: 'left',
                 },
