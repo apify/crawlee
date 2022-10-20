60f71933c722811ce419ac9bd3265905b68c4f8b
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2022-10-20 11:31:46 +0200

    Revert purgeOnStart check

--- packages/basic-crawler/src/internals/basic-crawler.ts
+++ packages/basic-crawler/src/internals/basic-crawler.ts
@@ -598,13 +598,11 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
         await this._init();
         await this.stats.startCapturing();
 
-        if (!this.config.get('purgeOnStart')) {
-            process.once('SIGINT', async () => {
-                this.log.warning('Pausing... Press CTRL+C again to force exit. To resume, do: CRAWLEE_PURGE_ON_START=0 npm run start');
-                await this._pauseOnMigration();
-                await this.autoscaledPool!.abort();
-            });
-        }
+        process.once('SIGINT', async () => {
+            this.log.warning('Pausing... Press CTRL+C again to force exit. To resume, do: CRAWLEE_PURGE_ON_START=0 npm run start');
+            await this._pauseOnMigration();
+            await this.autoscaledPool!.abort();
+        });
 
         try {
             this.log.info('Starting the crawl');
