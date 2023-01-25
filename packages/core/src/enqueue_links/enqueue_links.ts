4ba982a909a3c16004b24ef90c3da3ee4e075be0
Vlad Frangu <kingdgrizzle@gmail.com>
2023-01-25 17:29:53 +0200

    feat(enqueueLinks): add SameOrigin strategy and relax protocol matching for the other strategies (#1748)
    Also fixes documentation for SameHostname and SameOrigin to actually
    match what the code does (they were a bit inverted and outdated)

--- packages/core/src/enqueue_links/enqueue_links.ts
+++ packages/core/src/enqueue_links/enqueue_links.ts
@@ -115,11 +115,46 @@ export interface EnqueueLinksOptions extends RequestQueueOperationOptions {
 
     /**
      * The strategy to use when enqueueing the urls.
+     *
+     * Depending on the strategy you select, we will only check certain parts of the URLs found. Here is a diagram of each URL part and their name:
+     *
+     * ```md
+     * Protocol          Domain
+     * ┌────┐          ┌─────────┐
+     * https://example.crawlee.dev/...
+     * │       └─────────────────┤
+     * │             Hostname    │
+     * │                         │
+     * └─────────────────────────┘
+     *          Origin
+     *```
+     *
      * @default EnqueueStrategy.SameHostname
      */
-    strategy?: EnqueueStrategy | 'all' | 'same-domain' | 'same-hostname';
+    strategy?: EnqueueStrategy | 'all' | 'same-domain' | 'same-hostname' | 'same-origin';
 }
 
+/**
+ * The different enqueueing strategies available.
+ *
+ * Depending on the strategy you select, we will only check certain parts of the URLs found. Here is a diagram of each URL part and their name:
+ *
+ * ```md
+ * Protocol          Domain
+ * ┌────┐          ┌─────────┐
+ * https://example.crawlee.dev/...
+ * │       └─────────────────┤
+ * │             Hostname    │
+ * │                         │
+ * └─────────────────────────┘
+ *          Origin
+ *```
+ *
+ * - The `Protocol` is usually `http` or `https`
+ * - The `Domain` represents the path without any possible subdomains to a website. For example, `crawlee.dev` is the domain of `https://example.crawlee.dev/`
+ * - The `Hostname` is the full path to a website, including any subdomains. For example, `example.crawlee.dev` is the hostname of `https://example.crawlee.dev/`
+ * - The `Origin` is the combination of the `Protocol` and `Hostname`. For example, `https://example.crawlee.dev` is the origin of `https://example.crawlee.dev/`
+ */
 export enum EnqueueStrategy {
     /**
      * Matches any URLs found
@@ -128,17 +163,30 @@ export enum EnqueueStrategy {
 
     /**
      * Matches any URLs that have the same hostname.
-     * For example, `https://wow.example.com/hello` will be matched for a base url of `https://example.com/`, but
+     * For example, `https://wow.example.com/hello` will be matched for a base url of `https://wow.example.com/`, but
      * `https://example.com/hello` will not be matched.
+     *
+     * > This strategy will match both `http` and `https` protocols regardless of the base URL protocol.
      */
     SameHostname = 'same-hostname',
 
     /**
-     * Matches any URLs that have the same (sub-)domain as the base URL.
+     * Matches any URLs that have the same domain as the base URL.
      * For example, `https://wow.an.example.com` and `https://example.com` will both be matched for a base url of
      * `https://example.com`.
+     *
+     * > This strategy will match both `http` and `https` protocols regardless of the base URL protocol.
      */
     SameDomain = 'same-domain',
+
+    /**
+     * Matches any URLs that have the same hostname and protocol.
+     * For example, `https://wow.example.com/hello` will be matched for a base url of `https://wow.example.com/`, but
+     * `http://wow.example.com/hello` will not be matched.
+     *
+     * > This strategy will ensure the protocol of the base URL is the same as the protocol of the URL to be enqueued.
+     */
+    SameOrigin = 'same-origin',
 }
 
 /**
@@ -196,11 +244,7 @@ export async function enqueueLinks(options: SetRequired<EnqueueLinksOptions, 're
             ow.object.hasKeys('regexp'),
         )),
         transformRequestFunction: ow.optional.function,
-        strategy: ow.optional.string.oneOf([
-            EnqueueStrategy.All,
-            EnqueueStrategy.SameHostname,
-            EnqueueStrategy.SameDomain,
-        ]),
+        strategy: ow.optional.string.oneOf(Object.values(EnqueueStrategy)),
     }));
 
     const {
@@ -243,7 +287,7 @@ export async function enqueueLinks(options: SetRequired<EnqueueLinksOptions, 're
                 // We need to get the origin of the passed in domain in the event someone sets baseUrl
                 // to an url like https://example.com/deep/default/path and one of the found urls is an
                 // absolute relative path (/path/to/page)
-                enqueueStrategyPatterns.push({ glob: `${url.origin}/**` });
+                enqueueStrategyPatterns.push({ glob: ignoreHttpSchema(`${url.origin}/**`) });
                 break;
             case EnqueueStrategy.SameDomain: {
                 // Get the actual hostname from the base url
@@ -253,15 +297,20 @@ export async function enqueueLinks(options: SetRequired<EnqueueLinksOptions, 're
                     // We have a hostname, so we can use it to match all links on the page that point to it and any subdomains of it
                     url.hostname = baseUrlHostname;
                     enqueueStrategyPatterns.push(
-                        { glob: `${url.origin.replace(baseUrlHostname, `*.${baseUrlHostname}`)}/**` },
-                        { glob: `${url.origin}/**` },
+                        { glob: ignoreHttpSchema(`${url.origin.replace(baseUrlHostname, `*.${baseUrlHostname}`)}/**`) },
+                        { glob: ignoreHttpSchema(`${url.origin}/**`) },
                     );
                 } else {
                     // We don't have a hostname (can happen for ips for instance), so reproduce the same behavior
                     // as SameDomainAndSubdomain
-                    enqueueStrategyPatterns.push({ glob: `${url.origin}/**` });
+                    enqueueStrategyPatterns.push({ glob: ignoreHttpSchema(`${url.origin}/**`) });
                 }
 
+                break;
+            }
+            case EnqueueStrategy.SameOrigin: {
+                // The same behavior as SameHostname, but respecting the protocol of the URL
+                enqueueStrategyPatterns.push({ glob: `${url.origin}/**` });
                 break;
             }
             case EnqueueStrategy.All:
@@ -348,4 +397,11 @@ export interface ResolveBaseUrl {
     enqueueStrategy?: EnqueueLinksOptions['strategy'];
     originalRequestUrl: string;
     finalRequestUrl?: string;
+}
+
+/**
+ * Internal function that changes the enqueue globs to match both http and https
+ */
+function ignoreHttpSchema(pattern: string): string {
+    return pattern.replace(/^(https?):\/\//, 'http{s,}://');
 }
