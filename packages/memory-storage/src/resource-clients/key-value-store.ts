f30ca88d06a88a14e811d2fbd5ea9d4034817f0e
Vlad Frangu <kingdgrizzle@gmail.com>
2023-03-29 18:07:16 +0300

    fix(MemoryStorage): fix handling of readable streams

--- packages/memory-storage/src/resource-clients/key-value-store.ts
+++ packages/memory-storage/src/resource-clients/key-value-store.ts
@@ -217,7 +217,14 @@ export class KeyValueStoreClient extends BaseClient {
     async setRecord(record: storage.KeyValueStoreRecord): Promise<void> {
         s.object({
             key: s.string.lengthGreaterThan(0),
-            value: s.union(s.null, s.string, s.number, s.instance(Buffer), s.object({}).passthrough),
+            value: s.union(
+                s.null,
+                s.string,
+                s.number,
+                s.instance(Buffer),
+                s.instance(ArrayBuffer),
+                s.typedArray(),
+                s.object({}).passthrough),
             contentType: s.string.lengthGreaterThan(0).optional,
         }).parse(record);
 
---
+++ packages/memory-storage/test/key-value-store-stream.test.ts
@@ -0,0 +1,22 @@
+import { MemoryStorage } from '@crawlee/memory-storage';
+import { Readable } from 'node:stream';
+
+describe('KeyValueStore should drain streams when setting records', () => {
+    const storage = new MemoryStorage({
+        persistStorage: false,
+    });
+
+    const fsStream = Readable.from([Buffer.from('hello'), Buffer.from('world')]);
+
+    test('should drain stream', async () => {
+        const defaultStoreInfo = await storage.keyValueStores().getOrCreate('default');
+        const defaultStore = storage.keyValueStore(defaultStoreInfo.id);
+
+        await defaultStore.setRecord({ key: 'streamz', value: fsStream, contentType: 'text/plain' });
+
+        expect(fsStream.destroyed).toBeTruthy();
+
+        const record = await defaultStore.getRecord('streamz');
+        expect(record!.value.toString('utf8')).toEqual('helloworld');
+    });
+});
