f649c9dbdae86cde45689c0277267d000b0bf7a7
Vlad Frangu <kingdgrizzle@gmail.com>
2023-04-25 15:58:20 +0300

    test: add RequestQueue lruCache test (#1876)
    Added to help with this internal convo:
    https://apifier.slack.com/archives/C01E7CSJ2PP/p1681821495797309

---
+++ packages/core/test/request-queue/adding-the-same-request-should-not-call-the-api.test.ts
@@ -0,0 +1,52 @@
+import { MemoryStorage } from '@crawlee/memory-storage';
+import type { RequestQueueInfo } from '@crawlee/types';
+import { Configuration, RequestQueue } from 'crawlee';
+
+const originalClient = Configuration.getStorageClient();
+Configuration.useStorageClient(new MemoryStorage({ persistStorage: false, writeMetadata: false }));
+
+afterAll(() => {
+    Configuration.useStorageClient(originalClient);
+});
+
+let requestQueueInfo: RequestQueueInfo;
+
+beforeAll(async () => {
+    requestQueueInfo = await Configuration.getStorageClient().requestQueues().getOrCreate('test-request-queue-not-called-on-cached-request');
+});
+
+describe('RequestQueue#addRequest should not call the API if the request is already in the queue', () => {
+    test('should not call the API if the request is already in the queue', async () => {
+        const requestQueue = new RequestQueue({ id: requestQueueInfo.id, client: Configuration.getStorageClient() });
+
+        const clientSpy = jest.spyOn(requestQueue.client, 'addRequest');
+
+        const requestData = await requestQueue.addRequest({ url: 'https://example.com' });
+
+        expect(clientSpy).toHaveBeenCalledTimes(1);
+
+        await requestQueue.markRequestHandled({ id: requestData.requestId, uniqueKey: requestData.uniqueKey } as any);
+
+        await requestQueue.addRequest({ url: 'https://example.com' });
+
+        expect(clientSpy).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('RequestQueue#addRequests should not call the API if the request is already in the queue', () => {
+    test('should not call the API if the request is already in the queue', async () => {
+        const requestQueue = new RequestQueue({ id: requestQueueInfo.id, client: Configuration.getStorageClient() });
+
+        const clientSpy = jest.spyOn(requestQueue.client, 'batchAddRequests');
+
+        const requestData = await requestQueue.addRequests([{ url: 'https://example2.com' }]);
+
+        expect(clientSpy).toHaveBeenCalledTimes(1);
+
+        await requestQueue.markRequestHandled({ id: requestData.processedRequests[0].requestId, uniqueKey: requestData.processedRequests[0].uniqueKey } as any);
+
+        await requestQueue.addRequests([{ url: 'https://example2.com' }]);
+
+        expect(clientSpy).toHaveBeenCalledTimes(1);
+    });
+});
