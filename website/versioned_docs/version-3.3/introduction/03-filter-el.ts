65969cd0733925953ebca7ee5fe2a14100eac214
Eirik Mo <eirik@mowebdev.com>
2023-04-28 16:56:26 +0700

    fix: add code comment for why no params for v3.3

--- website/versioned_docs/version-3.3/introduction/03-filter-el.ts
+++ website/versioned_docs/version-3.3/introduction/03-filter-el.ts
@@ -5,6 +5,9 @@ const crawler = new CheerioCrawler({
     async requestHandler({ $, request, enqueueLinks }) {
         const title = $('title').text();
         console.log(`The title of "${request.url}" is: ${title}.`);
+        // The default behavior of enqueueLinks is to stay on the same hostname,
+        // so it does not require any parameters.
+        // This will ensure the subdomain stays the same.
         await enqueueLinks();
     },
 });
