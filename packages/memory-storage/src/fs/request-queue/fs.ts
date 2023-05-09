063dcd1c9e6652cd316cc0e8c4f4e4bbb70c246e
Vlad Frangu <kingdgrizzle@gmail.com>
2023-05-09 16:49:42 +0300

    fix(MemoryStorage): cache requests in `RequestQueue` (#1899)
    This is added because request queues tend to have a _lot_ of activity on
    one entry, until the request is finished. This should ensure that no
    more invalid JSON errors are thrown, while also keeping the memory
    footprint low once a request entry is no longer accessed (+re-loading
    from fs if that happens)

--- packages/memory-storage/src/fs/request-queue/fs.ts
+++ packages/memory-storage/src/fs/request-queue/fs.ts
@@ -10,6 +10,13 @@ import type { StorageImplementation } from '../common';
 export class RequestQueueFileSystemEntry implements StorageImplementation<InternalRequest> {
     private filePath: string;
     private fsQueue = new AsyncQueue();
+    private data?: InternalRequest;
+
+    /**
+     * A "sweep" timeout that is created/refreshed whenever this entry is accessed/updated.
+     * It exists to ensure that the entry is not kept in memory indefinitely, by sweeping it after 60 seconds of inactivity (in order to keep memory usage low)
+     */
+    private sweepTimeout?: NodeJS.Timeout;
 
     constructor(options: CreateStorageImplementationOptions) {
         this.filePath = resolve(options.storeDirectory, `${options.requestId}.json`);
@@ -17,8 +24,18 @@ export class RequestQueueFileSystemEntry implements StorageImplementation<Intern
 
     async get() {
         await this.fsQueue.wait();
+        this.setOrRefreshSweepTimeout();
+
+        if (this.data) {
+            this.fsQueue.shift();
+            return this.data;
+        }
+
         try {
-            return JSON.parse(await readFile(this.filePath, 'utf-8'));
+            const req = JSON.parse(await readFile(this.filePath, 'utf-8'));
+            this.data = req;
+
+            return req;
         } finally {
             this.fsQueue.shift();
         }
@@ -26,10 +43,13 @@ export class RequestQueueFileSystemEntry implements StorageImplementation<Intern
 
     async update(data: InternalRequest) {
         await this.fsQueue.wait();
+        this.data = data;
+
         try {
             await ensureDir(dirname(this.filePath));
             await lockAndWrite(this.filePath, data);
         } finally {
+            this.setOrRefreshSweepTimeout();
             this.fsQueue.shift();
         }
     }
@@ -38,5 +58,16 @@ export class RequestQueueFileSystemEntry implements StorageImplementation<Intern
         await this.fsQueue.wait();
         await rm(this.filePath, { force: true });
         this.fsQueue.shift();
+    }
+
+    private setOrRefreshSweepTimeout() {
+        if (this.sweepTimeout) {
+            this.sweepTimeout.refresh();
+        } else {
+            this.sweepTimeout = setTimeout(() => {
+                this.sweepTimeout = undefined;
+                this.data = undefined;
+            }, 60_000).unref();
+        }
     }
 }
