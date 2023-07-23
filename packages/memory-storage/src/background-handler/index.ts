8de69f57a61f9dad4c614f3deacb1957fc5cb31d
Vlad Frangu <kingdgrizzle@gmail.com>
2023-07-24 00:50:02 +0300

    chore: document promiseMap

--- packages/memory-storage/src/background-handler/index.ts
+++ packages/memory-storage/src/background-handler/index.ts
@@ -3,6 +3,11 @@ import { randomUUID } from 'node:crypto';
 import { handleMessage } from './fs-utils';
 import type { BackgroundHandlerReceivedMessage } from '../utils';
 
+/**
+ * A map of promises that are created when a background task is scheduled.
+ * This is used in MemoryStorage#teardown to wait for all tasks to finish executing before exiting the process.
+ * @internal
+ */
 export const promiseMap: Map<string, {
     promise: Promise<void>;
     resolve: () => void;
