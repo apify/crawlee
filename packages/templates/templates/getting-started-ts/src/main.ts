20bd3a8f24ec18c41ba45f94f56ac38dc6e36196
Jindřich Bär <jindrichbar@gmail.com>
2023-08-31 13:43:52 +0200

    chore: templates use new dataset helpers

--- packages/templates/templates/getting-started-ts/src/main.ts
+++ packages/templates/templates/getting-started-ts/src/main.ts
@@ -1,16 +1,16 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, Dataset } from 'crawlee';
+import { PlaywrightCrawler } from 'crawlee';
 
 // PlaywrightCrawler crawls the web using a headless
 // browser controlled by the Playwright library.
 const crawler = new PlaywrightCrawler({
     // Use the requestHandler to process each of the crawled pages.
-    async requestHandler({ request, page, enqueueLinks, log }) {
+    async requestHandler({ request, page, enqueueLinks, log, pushData }) {
         const title = await page.title();
         log.info(`Title of ${request.loadedUrl} is '${title}'`);
 
         // Save results as JSON to ./storage/datasets/default
-        await Dataset.pushData({ title, url: request.loadedUrl });
+        await pushData({ title, url: request.loadedUrl });
 
         // Extract links from the current page
         // and add them to the crawling queue.
