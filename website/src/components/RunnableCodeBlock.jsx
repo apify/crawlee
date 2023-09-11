374d26a3f8fadb43b1938edd8b8d311e39faac88
Josef Válek <valek.josef@seznam.cz>
2023-09-11 18:50:19 +0200

    Update runnable code block component

--- website/src/components/RunnableCodeBlock.jsx
+++ website/src/components/RunnableCodeBlock.jsx
@@ -6,59 +6,37 @@ import useIsBrowser from '@docusaurus/useIsBrowser';
 import styles from './RunnableCodeBlock.module.css';
 // import axios from 'axios';
 
-const RunnableCodeBlock = ({ children: example, ...props }) => {
-    const isBrowser = useIsBrowser();
-    // let href = 'https://console.apify.com/actor/adamek~example-code-runner?runConfig={response.data.encoded}';
-    let href = 'https://console.apify.com/actor/adamek~example-code-runner';
+const useCodeHash = (example) => {
     const [hash, setHash] = useState('');
+    const isBrowser = useIsBrowser();
+    useEffect(() => {
+        if (isBrowser) return;
+        (async () => {
+            // TODO: Use proper token
+            const res = await fetch('https://api.apify.com/v2/tools/encode-and-sign?token=XXX', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                // TODO: Send proper payload
+                body: JSON.stringify({ code: example }),
+            });
+            const encoded = await res.json();
+            setHash(encoded);
+        })();
+    }, [isBrowser, example]);
+    return hash;
+};
 
-    // useEffect(() => {
-    //     async function getHash() {
-    //         const response = await axios.post(
-    //             'https://api.apify.com/tools/encode-and-sign',
-    //             { code: example },
-    //             {
-    //                 headers: { Authorization: '...' },
-    //             },
-    //         );
-    //         console.log(response);
-    //         console.log(response.json);
-    //         const data = await response.json;
-    //         console.log(data);
-    //         setHash(data.access_token);
-    //     }
-    //
-    //     // You need to restrict it at some point
-    //     // This is just dummy code and should be replaced by actual
-    //     if (!hash) {
-    //         void getHash();
-    //     }
-    // }, [hash]);
-
-    if (isBrowser) {
-        // const parts = urlHash.split(CodeHashManager.SECTION_SEPARATOR)
-        //     .map((part) => {
-        //         return part.replace(/\+/g, '-')
-        //             .replace(/\//g, '_')
-        //             .replace(/=+$/m, '');
-        //     });
-        // href += `?runConfig=${parts.join(CodeHashManager.SECTION_SEPARATOR)}`;
-    } else {
-        const { CodeHashManager } = require('@apify/utilities');
-        const manager = new CodeHashManager('...TODO...');
-        const urlHash = manager.encode(example, 'EgPtw3oej6TaDt5qn');
-        // const res = await fetch('https://api.apify.com/tools/encode-and-sign', {
-        //     method: 'POST',
-        //     body: {
-        //         code: example,
-        //     },
-        // });
-        // console.log(res);
-        href += `?runConfig=${urlHash}`;
-    }
+const RunnableCodeBlock = ({ children: example, ...props }) => {
+    // TODO: Use hash as runConfig parameter
+    // let href = 'https://console.apify.com/actor/adamek~example-code-runner?runConfig={response.data.encoded}';
+    let href = 'https://console.apify.com/v1/actor/adamek~example-code-runner';
+    const hash = useCodeHash(example);
 
     return (
         <div className={clsx(styles.container, 'runnable-code-block')}>
+            <code>{JSON.stringify(hash)}</code>
             <Link href={href} className={styles.button} rel="follow">Run on Apify</Link>
             <CodeBlock {...props} className={clsx(styles.codeBlock, 'code-block')}>
                 {example}
