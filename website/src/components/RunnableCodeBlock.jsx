b03e1d77460945309342a51a0819b24eb7c8181d
Martin Adámek <banan23@gmail.com>
2023-09-12 13:40:17 +0200

    WIP

--- website/src/components/RunnableCodeBlock.jsx
+++ website/src/components/RunnableCodeBlock.jsx
@@ -4,22 +4,25 @@ import CodeBlock from '@theme/CodeBlock';
 import Link from '@docusaurus/Link';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import styles from './RunnableCodeBlock.module.css';
-// import axios from 'axios';
 
 const useCodeHash = (example) => {
     const [hash, setHash] = useState('');
     const isBrowser = useIsBrowser();
     useEffect(() => {
+        console.log('wut', { isBrowser });
         if (isBrowser) return;
         (async () => {
-            // TODO: Use proper token
-            const res = await fetch('https://api.apify.com/v2/tools/encode-and-sign?token=XXX', {
+            const token = process.env.APIFY_ADMIN_TOKEN;
+            console.log('wtf', token, process.env);
+            const res = await fetch(`https://api.apify.com/v2/tools/encode-and-sign?token=${token}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                // TODO: Send proper payload
-                body: JSON.stringify({ code: example }),
+                body: JSON.stringify({
+                    input: JSON.stringify({ code: example }),
+                    options: { contentType: 'application/json', timeout: 300, memory: 1024, maxItems: 1000 },
+                }),
             });
             const encoded = await res.json();
             setHash(encoded);
@@ -29,14 +32,17 @@ const useCodeHash = (example) => {
 };
 
 const RunnableCodeBlock = ({ children: example, ...props }) => {
-    // TODO: Use hash as runConfig parameter
-    // let href = 'https://console.apify.com/actor/adamek~example-code-runner?runConfig={response.data.encoded}';
-    let href = 'https://console.apify.com/v1/actor/adamek~example-code-runner';
+    let href = 'https://console.apify.com/actors/1ZCFfY19U64UYWwS0';
     const hash = useCodeHash(example);
+
+    if (hash) {
+        href += `?runConfig=${hash.data.encoded}`;
+    } else {
+        href += `?runConfig=invalid-token`;
+    }
 
     return (
         <div className={clsx(styles.container, 'runnable-code-block')}>
-            <code>{JSON.stringify(hash)}</code>
             <Link href={href} className={styles.button} rel="follow">Run on Apify</Link>
             <CodeBlock {...props} className={clsx(styles.codeBlock, 'code-block')}>
                 {example}
