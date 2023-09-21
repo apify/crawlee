2d69ff0ee199767451232302f107ba712daa7359
Jindřich Bär <jindrichbar@gmail.com>
2023-09-21 12:51:27 +0200

    fix: broken tests

--- test/core/crawlers/cheerio_crawler.test.ts
+++ test/core/crawlers/cheerio_crawler.test.ts
@@ -1150,7 +1150,7 @@ describe('CheerioCrawler', () => {
                 // localhost proxy causes proxy errors, session rotations and finally throws, but we don't care
             }
 
-            expect(newUrlSpy).toBeCalledWith(usedSession.id);
+            expect(newUrlSpy).toBeCalledWith(usedSession.id, expect.anything());
         });
     });
 
