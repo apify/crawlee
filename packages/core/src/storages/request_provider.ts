c49d0ca4f6732173daef8066489976d763962bca
Jindřich Bär <jindrichbar@gmail.com>
2023-09-21 14:24:38 +0200

    chore: merge conflict resolve

--- packages/core/src/storages/request_provider.ts
+++ packages/core/src/storages/request_provider.ts
@@ -558,7 +558,11 @@ export abstract class RequestProvider implements IStorage {
         // Download remote resource and parse URLs.
         let urlsArr;
         try {
-            urlsArr = await this._downloadListOfUrls({ url: requestsFromUrl, urlRegExp: regex, proxyUrl: await this.proxyConfiguration?.newUrl() });
+            urlsArr = await this._downloadListOfUrls({
+                url: requestsFromUrl,
+                urlRegExp: regex,
+                proxyUrl: await this.proxyConfiguration?.newUrl() || undefined,
+            });
         } catch (err) {
             throw new Error(`Cannot fetch a request list from ${requestsFromUrl}: ${err}`);
         }
