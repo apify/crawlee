a2ec6eda836370df42224e94c35925b4bd16f14a
Vlad Frangu <kingdgrizzle@gmail.com>
2023-10-05 14:33:31 +0300

    chore: initial starts, nothing works but things happen

---
+++ vitest.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'node:path';
+
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    plugins: [tsconfigPaths()],
+    esbuild: {
+        target: 'es2021',
+        format: 'cjs',
+    },
+    test: {
+        globals: true,
+        coverage: {
+            provider: 'v8',
+            enabled: true,
+            reporter: ['text', 'lcov', 'cobertura'],
+            exclude: [
+                '**/node_modules/**',
+                '**/dist/**',
+                '**/test/**',
+            ],
+        },
+        // minThreads: 1,
+        // maxThreads: 3,
+        testTimeout: 60_000,
+        alias: [
+            { find: '@crawlee/browser-pool', replacement: resolve(__dirname, 'packages/browser-pool/src/index.ts') },
+        ],
+    },
+});
