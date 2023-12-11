bf9a2edc763c412382a2a011232e75bc75287c0d
Jan Buchar <Teyras@gmail.com>
2023-12-11 14:36:46 +0100

    Simplify RobotsFile and Sitemap API

--- packages/utils/src/internals/robots.ts
+++ packages/utils/src/internals/robots.ts
@@ -1,11 +1,10 @@
-import log from '@apify/log';
 // @ts-expect-error This throws a compilation error due to got-scraping being ESM only but we only import types, so its alllll gooooood
 import type { HTTPError as HTTPErrorClass } from 'got-scraping';
 import type { Robot } from 'robots-parser';
 import robotsParser from 'robots-parser';
-import sax from 'sax';
 
 import { gotScraping } from './gotScraping';
+import { Sitemap } from './sitemap';
 
 let HTTPError: typeof HTTPErrorClass;
 
@@ -53,74 +52,11 @@ export class RobotsFile {
         return this.robots.getSitemaps();
     }
 
-    async parseSitemaps(): Promise<Sitemap[]> {
-        return Promise.all(this.robots.getSitemaps().map(async (sitemap) => Sitemap.load(sitemap, this.proxyUrl)));
+    async parseSitemaps(): Promise<Sitemap> {
+        return Sitemap.load(this.robots.getSitemaps(), this.proxyUrl);
     }
 
     async parseUrlsFromSitemaps(): Promise<string[]> {
-        return (await this.parseSitemaps()).flatMap((sitemap) => sitemap.urls);
-    }
-}
-
-export class Sitemap {
-    constructor(readonly urls: string[]) {}
-
-    static async load(url: string, proxyUrl?: string): Promise<Sitemap> {
-        const parsingState: {
-            context: 'sitemapindex' | 'urlset' | undefined;
-            loc: boolean;
-            urls: string[];
-            sitemapUrls: string[];
-            visitedSitemapUrls: string[];
-        } = { context: undefined, loc: false, urls: [], sitemapUrls: [url], visitedSitemapUrls: [] };
-
-        while (parsingState.sitemapUrls.length > 0) {
-            const sitemapUrl = parsingState.sitemapUrls.pop()!;
-            parsingState.visitedSitemapUrls.push(sitemapUrl);
-
-            const response = await gotScraping({ url: sitemapUrl, proxyUrl, responseType: 'text', method: 'GET' });
-            const parser = sax.parser(true);
-
-            parser.onopentag = (node) => {
-                if (node.name === 'loc' && parsingState.context !== undefined) {
-                    parsingState.loc = true;
-                }
-                if (node.name === 'urlset') {
-                    parsingState.context = 'urlset';
-                }
-                if (node.name === 'sitemapindex') {
-                    parsingState.context = 'sitemapindex';
-                }
-            };
-
-            parser.onclosetag = (name) => {
-                if (name === 'loc') {
-                    parsingState.loc = false;
-                }
-            };
-
-            parser.ontext = (text) => {
-                if (parsingState.loc) {
-                    if (parsingState.context === 'sitemapindex') {
-                        if (!parsingState.visitedSitemapUrls.includes(text)) {
-                            parsingState.sitemapUrls.push(text);
-                        }
-                    }
-                    if (parsingState.context === 'urlset') {
-                        parsingState.urls.push(text);
-                    }
-                }
-            };
-
-            if (response.statusCode === 200) {
-                try {
-                    parser.write(response.body).close();
-                } catch {
-                    log.warning(`Malformed sitemap content: ${url}`);
-                }
-            }
-        }
-
-        return new Sitemap(parsingState.urls);
+        return (await this.parseSitemaps()).urls;
     }
 }
