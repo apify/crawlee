f091c7946ba9d64fbf4f2b88d9cce56548163964
Vlad Frangu <kingdgrizzle@gmail.com>
2023-12-17 22:46:54 +0200

    chore: bite me undefined

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -250,7 +250,9 @@ export class Request<UserData extends Dictionary = Dictionary> {
         if (maxRetries != null) this.maxRetries = maxRetries;
 
         // If it's already set, don't override it (for instance when fetching from storage)
-        this.enqueueStrategy = this.enqueueStrategy ?? enqueueStrategy;
+        if (!this.enqueueStrategy && enqueueStrategy) {
+            this.enqueueStrategy = this.enqueueStrategy ?? enqueueStrategy;
+        }
     }
 
     /** Tells the crawler processing this request to skip the navigation and process the request directly. */
