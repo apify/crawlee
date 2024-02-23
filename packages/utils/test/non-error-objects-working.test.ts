e08a3cfb40ae82924582027834c4d1e2b02c6087
Hamza Alwan <ihamzaalwan@gmail.com>
2024-02-23 17:41:40 +0300

    fix: lint issue

--- packages/utils/test/non-error-objects-working.test.ts
+++ packages/utils/test/non-error-objects-working.test.ts
@@ -4,6 +4,6 @@ describe('ErrorTracker', () => {
     test('processing a non-error error should not crash', () => {
         const errorTracker = new ErrorTracker();
         // @ts-expect-error tracking non-error errors
-        expect(async () => await errorTracker.add('foo')).not.toThrow();
+        expect(() => errorTracker.add('foo')).not.toThrow();
     });
 });
