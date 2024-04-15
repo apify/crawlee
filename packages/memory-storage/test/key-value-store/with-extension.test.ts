613cf6a3fcde663bfdfe92bc7a092bf5c7be3584
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2024-04-15 12:45:35 +0000

    fix: fix linting issues

--- packages/memory-storage/test/key-value-store/with-extension.test.ts
+++ packages/memory-storage/test/key-value-store/with-extension.test.ts
@@ -1,34 +1,34 @@
-import { createKeyValueStorageImplementation} from "packages/memory-storage/src/fs/key-value-store";
-
-import { existsSync, emptyDirSync } from 'fs-extra';
 import { resolve } from 'node:path';
 
-describe('KeyValueStore should append extension only when needed', () => {
-    const mockImageBuffer = Buffer.from("This is a test image", "utf8");
+import { existsSync, emptyDirSync } from 'fs-extra';
+import { createKeyValueStorageImplementation } from 'packages/memory-storage/src/fs/key-value-store';
 
-    afterAll(() => emptyDirSync("tmp"));
-    
+describe('KeyValueStore should append extension only when needed', () => {
+    const mockImageBuffer = Buffer.from('This is a test image', 'utf8');
+
+    afterAll(() => emptyDirSync('tmp'));
+
     test('should append extension when needed', async () => {
-        const testDir = resolve("tmp", "test_no_extension");
+        const testDir = resolve('tmp', 'test_no_extension');
         const storage = createKeyValueStorageImplementation({
             persistStorage: true,
             storeDirectory: testDir,
-            writeMetadata: true
+            writeMetadata: true,
         });
-        await storage.update({ key: 'jibberish', value: mockImageBuffer, contentType: 'image/jpeg', extension: "jpeg" });
+        await storage.update({ key: 'jibberish', value: mockImageBuffer, contentType: 'image/jpeg', extension: 'jpeg' });
 
         expect(existsSync(resolve(testDir, 'jibberish.jpeg'))).toBeTruthy();
         expect(existsSync(resolve(testDir, 'jibberish'))).toBeFalsy();
     });
 
     test('should not append extension when already available', async () => {
-        const testDir = resolve("tmp", "test_extension");
+        const testDir = resolve('tmp', 'test_extension');
         const storage = createKeyValueStorageImplementation({
             persistStorage: true,
             storeDirectory: testDir,
-            writeMetadata: true
+            writeMetadata: true,
         });
-        await storage.update({ key: 'jibberish.jpg', value: mockImageBuffer, contentType: 'image/jpeg', extension: "jpeg" });
+        await storage.update({ key: 'jibberish.jpg', value: mockImageBuffer, contentType: 'image/jpeg', extension: 'jpeg' });
 
         expect(existsSync(resolve(testDir, 'jibberish.jpg'))).toBeTruthy();
         expect(existsSync(resolve(testDir, 'jibberish.jpg.jpeg'))).toBeFalsy();
