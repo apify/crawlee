apify/crawlee

commit 9e6eb1e32f582a8837311aac12cc1d657432f3fa

feat: allow running single crawler instance multiple times (#1844)
Often users are trying to reuse a single crawler instance, which wasn't
possible due to the internal state not being reset in the `run` method
completely. One example of this was the use inside express request
handler.

This PR adds support for doing so, by resetting the crawler stats and
default request queue. If the crawler is already running, an error is
thrown, suggesting the use of `crawler.addRequests()` instead.

Related: #765

packages/basic-crawler/src/internals/basic-crawler.ts
@@ -369,6 +369,8 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
      */
     readonly router: RouterHandler<Context> = Router.create<Context>();
 
+    running?: boolean;
+
     protected log: Log;
     protected requestHandler!: RequestHandler<Context>;
     protected errorHandler?: ErrorHandler<Context>;
@@ -651,7 +653,26 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
      * @param [requests] The requests to add
      * @param [options] Options for the request queue
      */
-    async run(requests?: (string | Request | RequestOptions)[], options?: CrawlerAddRequestsOptions): Promise<FinalStatistics> {
+    async run(requests?: (string | Request | RequestOptions)[], options?: CrawlerRunOptions): Promise<FinalStatistics> {
+        if (this.running) {
+            throw new Error('This crawler instance is already running, you can add more requests to it via `crawler.addRequests()`.');
+        }
+
+        const purgeRequestQueue = options?.purgeRequestQueue ?? true;
+
+        // When executing the run method for the second time explicitly (the first time `this.running` is `undefined`),
+        // we need to purge the default RQ to allow processing the same requests again - this is important so users can
+        // pass in failed requests back to the `crawler.run()`, otherwise they would be considered as handled and
+        // ignored - as a failed requests is still handled.
+        if (this.running === false && this.requestQueue?.name === 'default' && purgeRequestQueue) {
+            await this.requestQueue.drop();
+            this.requestQueue = await RequestQueue.open();
+        }
+
+        this.running = true;
+        this.stats.reset();
+        await this.stats.resetStore();
+
         await purgeDefaultStorages();
 
         if (requests) {
@@ -721,6 +742,8 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
         periodicLogger.stop();
         // eslint-disable-next-line max-len
         await this.setStatusMessage(`Finished! Total ${this.stats.state.requestsFinished + this.stats.state.requestsFailed} requests: ${this.stats.state.requestsFinished} succeeded, ${this.stats.state.requestsFailed} failed.`, { isStatusMessageTerminal: true });
+        this.running = false;
+
         return stats;
     }
 
@@ -1308,9 +1331,18 @@ export interface CreateContextOptions {
 export interface CrawlerAddRequestsOptions extends RequestQueueOperationOptions {
     /**
      * Whether to wait for all the provided requests to be added, instead of waiting just for the initial batch of up to 1000.
-     @default false
+     * @default false
      */
     waitForAllRequestsToBeAdded?: boolean;
+}
+
+export interface CrawlerRunOptions extends CrawlerAddRequestsOptions {
+    /**
+     * Whether to purge the RequestQueue before running the crawler again. Defaults to true, so it is possible to reprocess failed requests.
+     * When disabled, only new requests will be considered. Note that even a failed request is considered as handled.
+     * @default true
+     */
+    purgeRequestQueue?: boolean;
 }
 
 export interface CrawlerAddRequestsResult {

packages/core/src/crawlers/statistics.ts
@@ -148,6 +148,14 @@ export class Statistics {
         this._teardown();
     }
 
+    async resetStore() {
+        if (!this.keyValueStore) {
+            return;
+        }
+
+        await this.keyValueStore.setValue(this.persistStateKey, null);
+    }
+
     /**
      * Increments the status code counter.
      */

test/core/crawlers/basic_crawler.test.ts
@@ -108,6 +108,29 @@ describe('BasicCrawler', () => {
         expect(await requestList.isEmpty()).toBe(true);
     });
 
+    test('should allow using run method multiple times', async () => {
+        const sources = [...Array(100).keys()].map((index) => `https://example.com/${index}`);
+        const sourcesCopy = JSON.parse(JSON.stringify(sources));
+
+        const processed: { url: string }[] = [];
+        const requestHandler: RequestHandler = async ({ request }) => {
+            await sleep(10);
+            processed.push({ url: request.url });
+        };
+
+        const basicCrawler = new BasicCrawler({
+            minConcurrency: 25,
+            maxConcurrency: 25,
+            requestHandler,
+        });
+
+        await basicCrawler.run(sources);
+        await basicCrawler.run(sources);
+        await basicCrawler.run(sources);
+
+        expect(processed).toHaveLength(sourcesCopy.length * 3);
+    });
+
     test('should correctly combine shorthand and full length options', async () => {
         const shorthandOptions = {
             minConcurrency: 123,

test/e2e/playwright-multi-run/actor/.gitignore
@@ -1,0 +1,7 @@
+.idea
+.DS_Store
+node_modules
+package-lock.json
+apify_storage
+crawlee_storage
+storage

test/e2e/playwright-multi-run/actor/Dockerfile
@@ -1,0 +1,23 @@
+FROM node:16 AS builder
+
+COPY /packages ./packages
+COPY /package*.json ./
+RUN npm --quiet set progress=false \
+    && npm install --only=prod --no-optional \
+    && npm update
+
+FROM apify/actor-node-playwright-chrome:16-beta
+
+RUN rm -r node_modules
+COPY --from=builder /node_modules ./node_modules
+COPY --from=builder /packages ./packages
+COPY --from=builder /package*.json ./
+COPY /.actor ./.actor
+COPY /main.js ./
+
+RUN echo "Installed NPM packages:" \
+    && (npm list --only=prod --no-optional --all || true) \
+    && echo "Node.js version:" \
+    && node --version \
+    && echo "NPM version:" \
+    && npm --version

test/e2e/playwright-multi-run/actor/main.js
@@ -1,0 +1,27 @@
+import { Actor } from 'apify';
+import { Dataset, PlaywrightCrawler } from '@crawlee/playwright';
+import { ApifyStorageLocal } from '@apify/storage-local';
+
+if (process.env.STORAGE_IMPLEMENTATION === 'LOCAL') {
+    await Actor.init({ storage: new ApifyStorageLocal() });
+} else {
+    await Actor.init();
+}
+
+const crawler = new PlaywrightCrawler({
+    maxRequestsPerCrawl: 10,
+    async requestHandler({ page, enqueueLinks, request }) {
+        const { url } = request;
+        const pageTitle = await page.title();
+        await Dataset.pushData({ url, pageTitle });
+        await enqueueLinks({
+            globs: ['**/3.0/examples/*'],
+        });
+    },
+});
+
+await crawler.run(['https://crawlee.dev/docs/3.0/examples/']);
+await crawler.run(['https://crawlee.dev/docs/3.0/examples/']);
+await crawler.run(['https://crawlee.dev/docs/3.0/examples/']);
+
+await Actor.exit({ exit: Actor.isAtHome() });

test/e2e/playwright-multi-run/test.mjs
@@ -1,0 +1,10 @@
+import { initialize, getActorTestDir, runActor, expect, validateDataset } from '../tools.mjs';
+
+const testActorDirname = getActorTestDir(import.meta.url);
+await initialize(testActorDirname);
+
+const { datasetItems } = await runActor(testActorDirname, 16384);
+
+// we cant assert number of requests as the stats KVS is being wiped on each `run` call, unlike the dataset
+await expect(datasetItems.length > 30, 'Number of dataset items');
+await expect(validateDataset(datasetItems, ['url', 'pageTitle']), 'Dataset items validation');

commit 8ce7dd4ae6a3718dac95e784a53bd5661c827edc

fix: do not drop statistics on migration/resurrection/resume (#2462)
This fixes a bug that was introduced with
https://github.com/apify/crawlee/pull/1844 and
https://github.com/apify/crawlee/pull/2083 - we reset the persisted
state for statistics and session pool each time a crawler is started,
which prevents their restoration.

---------

Co-authored-by: Martin Adámek <banan23@gmail.com>

packages/basic-crawler/src/internals/basic-crawler.ts
@@ -470,7 +470,8 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
      */
     readonly router: RouterHandler<Context> = Router.create<Context>();
 
-    running?: boolean;
+    running = false;
+    hasFinishedBefore = false;
 
     protected log: Log;
     protected requestHandler!: RequestHandler<Context>;
@@ -829,19 +830,22 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
 
         const purgeRequestQueue = options?.purgeRequestQueue ?? true;
 
-        // When executing the run method for the second time explicitly (the first time `this.running` is `undefined`),
-        // we need to purge the default RQ to allow processing the same requests again - this is important so users can
-        // pass in failed requests back to the `crawler.run()`, otherwise they would be considered as handled and
-        // ignored - as a failed requests is still handled.
-        if (this.running === false && this.requestQueue?.name === 'default' && purgeRequestQueue) {
-            await this.requestQueue.drop();
-            this.requestQueue = await this._getRequestQueue();
+        if (this.hasFinishedBefore) {
+            // When executing the run method for the second time explicitly,
+            // we need to purge the default RQ to allow processing the same requests again - this is important so users can
+            // pass in failed requests back to the `crawler.run()`, otherwise they would be considered as handled and
+            // ignored - as a failed requests is still handled.
+            if (this.requestQueue?.name === 'default' && purgeRequestQueue) {
+                await this.requestQueue.drop();
+                this.requestQueue = await this._getRequestQueue();
+            }
+
+            this.stats.reset();
+            await this.stats.resetStore();
+            await this.sessionPool?.resetStore();
         }
 
         this.running = true;
-        this.stats.reset();
-        await this.stats.resetStore();
-        await this.sessionPool?.resetStore();
 
         await purgeDefaultStorages({ onlyPurgeOnce: true });
 
@@ -913,6 +917,7 @@ export class BasicCrawler<Context extends CrawlingContext = BasicCrawlingContext
         // eslint-disable-next-line max-len
         await this.setStatusMessage(`Finished! Total ${this.stats.state.requestsFinished + this.stats.state.requestsFailed} requests: ${this.stats.state.requestsFinished} succeeded, ${this.stats.state.requestsFailed} failed.`, { isStatusMessageTerminal: true, level: 'INFO' });
         this.running = false;
+        this.hasFinishedBefore = true;
 
         return stats;
     }

packages/playwright-crawler/src/internals/adaptive-playwright-crawler.ts
@@ -1,6 +1,13 @@
 import { addTimeoutToPromise } from '@apify/timeout';
 import { extractUrlsFromPage } from '@crawlee/browser';
-import type { RestrictedCrawlingContext, StatisticState, StatisticPersistedState, GetUserDataFromRequest, RouterRoutes } from '@crawlee/core';
+import type {
+    RestrictedCrawlingContext,
+    StatisticState,
+    StatisticsOptions,
+    StatisticPersistedState,
+    GetUserDataFromRequest,
+    RouterRoutes,
+} from '@crawlee/core';
 import { Configuration, RequestHandlerResult, Router, Statistics, withCheckedStorageAccess } from '@crawlee/core';
 import type { Awaitable, Dictionary } from '@crawlee/types';
 import { extractUrlsFromCheerio } from '@crawlee/utils';
@@ -27,6 +34,11 @@ interface AdaptivePlaywrightCrawlerPersistedStatisticState extends StatisticPers
 
 class AdaptivePlaywrightCrawlerStatistics extends Statistics {
     override state: AdaptivePlaywrightCrawlerStatisticState = null as any; // this needs to be assigned for a valid override, but the initialization is done by a reset() call from the parent constructor
+
+    constructor(options: StatisticsOptions = {}) {
+        super(options);
+        this.reset();
+    }
 
     override reset(): void {
         super.reset();