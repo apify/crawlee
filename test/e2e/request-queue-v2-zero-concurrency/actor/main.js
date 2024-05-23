1bb7712913040ef1a7c48c66d4f491abbccfd087
Martin Adámek <banan23@gmail.com>
2024-05-23 10:41:11 +0200

    test: add e2e test for zero concurrency with RQ v2

---
+++ test/e2e/request-queue-v2-zero-concurrency/actor/main.js
@@ -0,0 +1,33 @@
+import { CheerioCrawler, log, RequestQueue } from '@crawlee/cheerio';
+import { Actor } from 'apify';
+
+log.setLevel(log.LEVELS.DEBUG);
+
+process.env.CRAWLEE_INTERNAL_TIMEOUT = '30000';
+
+const mainOptions = {
+    exit: Actor.isAtHome(),
+    storage:
+        process.env.STORAGE_IMPLEMENTATION === 'LOCAL'
+            ? new (await import('@apify/storage-local')).ApifyStorageLocal()
+            : undefined,
+};
+
+// RequestQueue auto-reset when stuck with requests in progress
+await Actor.main(async () => {
+    const requestQueue = await RequestQueue.open();
+    await requestQueue.addRequest({ url: 'https://example.com/?q=1' });
+    await requestQueue.addRequest({ url: 'https://example.com/?q=2' });
+    const r3 = await requestQueue.addRequest({ url: 'https://example.com/?q=3' });
+    // trigger 0 concurrency by marking one of the requests as already in progress
+    requestQueue.inProgress.add(r3.requestId);
+
+    const crawler = new CheerioCrawler({
+        requestQueue,
+        async requestHandler({ request }) {
+            log.info(request.id);
+        },
+    });
+
+    await crawler.run();
+}, mainOptions);
