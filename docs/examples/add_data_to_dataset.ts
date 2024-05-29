2d40a4a9b8c02a60c51d5ba7d86ed1c1bb00e7d9
Saurav Jain <sauain@SauravApify.local>
2024-05-29 14:25:18 +0530

    improve add to dataset example

--- docs/examples/add_data_to_dataset.ts
+++ docs/examples/add_data_to_dataset.ts
@@ -1,10 +1,10 @@
-import { Dataset, CheerioCrawler } from 'crawlee';
+import { CheerioCrawler } from 'crawlee';
 
 const crawler = new CheerioCrawler({
     // Function called for each URL
     async requestHandler({ request, body }) {
         // Save data to default dataset
-        await Dataset.pushData({
+        await crawler.pushData({
             url: request.url,
             html: body,
         });
