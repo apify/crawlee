2812ed93b0bc6a9eb4f70bfa3a7ac5e74426ac99
Daniel Wébr <115245871+webrdaniel@users.noreply.github.com>
2024-09-03 09:18:27 +0200

    docs: add intercom messenger (#2651)
    Adding a script that loads Intercom messenger which we can use for AI
    chat bot.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -82,6 +82,20 @@ module.exports = {
             }),
         ],
     ]),
+    headTags: [
+        // Intercom messenger
+        {
+            tagName: 'script',
+            innerHTML: `window.intercomSettings={api_base:"https://api-iam.intercom.io",app_id:"kod1r788"};`,
+            attributes: {},
+        },
+        // Intercom messenger
+        {
+            tagName: 'script',
+            innerHTML: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/kod1r788';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`,
+            attributes: {},
+        },
+    ],
     plugins: [
         [
             'docusaurus-plugin-typedoc-api',
