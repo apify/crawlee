b420779ad12ff64f74e7802a7d47b8eaab0d1a25
Jindřich Bär <jindrichbar@gmail.com>
2025-01-20 15:54:55 +0100

    chore: remove unnecessary `console.log` from test

--- test/e2e/cheerio-impit-ts/test.mjs
+++ test/e2e/cheerio-impit-ts/test.mjs
@@ -12,7 +12,6 @@ const result = datasetItems[0];
 
 expect(result.body.length > 1000, 'HTML response is not empty');
 expect(result.title === 'httpbin.org', 'HTML title is correct');
-console.log(result.userAgent);
 expect(/Gecko\/\d{8} Firefox\/\d{2}/.test(result.userAgent), 'Impit correctly spoofs Firefox');
 expect(result.uuidJsonResponse.uuid !== undefined, 'JSON response contains UUID');
 expect(JSON.parse(result.uuidTextResponse).uuid !== undefined, 'Text response contains UUID');
