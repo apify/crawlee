705372fb9215a69f3efc0a4639faa564bfa6e702
Daniel Wébr <webrdaniel@gmail.com>
2025-02-28 09:52:59 +0100

    responsive navigation

---
+++ website/src/theme/DocBreadcrumbs/Items/Home/index.js
@@ -0,0 +1,25 @@
+import Link from '@docusaurus/Link';
+import { translate } from '@docusaurus/Translate';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import IconHome from '@theme/Icon/Home';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+export default function HomeBreadcrumbItem() {
+    const homeHref = useBaseUrl('/javascript');
+    return (
+        <li className="breadcrumbs__item">
+            <Link
+                aria-label={translate({
+                    id: 'theme.docs.breadcrumbs.home',
+                    message: 'Home page',
+                    description: 'The ARIA label for the home page in the breadcrumbs',
+                })}
+                className="breadcrumbs__link"
+                href={homeHref}>
+                <IconHome className={styles.breadcrumbHomeIcon} />
+            </Link>
+        </li>
+    );
+}
