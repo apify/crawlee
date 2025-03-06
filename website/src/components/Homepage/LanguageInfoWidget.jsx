8d67d6b4a963cafa94f33367eb5b9235f021eb64
Daniel Wébr <webrdaniel@gmail.com>
2025-03-06 15:06:01 +0100

    Language info widget

--- website/src/components/Homepage/LanguageInfoWidget.jsx
+++ website/src/components/Homepage/LanguageInfoWidget.jsx
@@ -1,3 +1,4 @@
+import ThemedImage from '@theme/ThemedImage';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -5,23 +6,38 @@ import Button from '../Button';
 import CopyButton from '../CopyButton';
 import styles from './LanguageInfoWidget.module.css';
 
-export default function LanguageInfoWidget({ language, command, githubUrl, to }) {
+export default function LanguageInfoWidget({
+    language,
+    command,
+    githubUrl,
+    to,
+}) {
     return (
         <div className={styles.languageGetStartedContainer}>
-            <div className={styles.logoContainer}>
-                <img
-                    alt="Crawlee Logo"
-                    width={32}
-                    height={32}
-                    src="/img/crawlee-logo.svg"
+            {language === 'JavaScript' && (
+                <ThemedImage
+                    sources={{
+                        light: '/img/crawlee-javascript-light.svg',
+                        dark: '/img/crawlee-javascript-dark.svg',
+                    }}
+                    alt="Crawlee JavaScript logo"
                 />
-                <div className={styles.languageGetStartedContainerTitle}>
-                    <span className={styles.crawleeText}>crawlee</span>
-                    <span className={styles.slashText}>/</span>
-                    <span className={styles.languageText}>{language}</span>
-                </div>
-            </div>
-            <div className={clsx(styles.buttonContainer, command && styles.buttonContainerVertical)}>
+            )}
+            {language === 'Python' && (
+                <ThemedImage
+                    sources={{
+                        light: '/img/crawlee-python-light.svg',
+                        dark: '/img/crawlee-python-dark.svg',
+                    }}
+                    alt="Crawlee Python logo"
+                />
+            )}
+            <div
+                className={clsx(
+                    styles.buttonContainer,
+                    command && styles.buttonContainerVertical,
+                )}
+            >
                 <Button to={to}>Learn more</Button>
                 <iframe
                     src={githubUrl}
@@ -31,7 +47,9 @@ export default function LanguageInfoWidget({ language, command, githubUrl, to })
                 ></iframe>
             </div>
             {command && (
-                <code className={styles.commandContainer}>{command} <CopyButton copyText={command} compact /></code>
+                <code className={styles.commandContainer}>
+                    {command} <CopyButton copyText={command} compact />
+                </code>
             )}
         </div>
     );
