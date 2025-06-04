b1c9b6a920ea29d436b5068a95e225e0b9f15f20
Martin Adámek <banan23@gmail.com>
2025-06-04 15:59:36 +0200

    chore: improve types to get rid of some `as any`

--- packages/puppeteer-crawler/src/internals/puppeteer-crawler.ts
+++ packages/puppeteer-crawler/src/internals/puppeteer-crawler.ts
@@ -4,7 +4,6 @@ import type {
     BrowserHook,
     BrowserRequestHandler,
     GetUserDataFromRequest,
-    LoadedContext,
     RouterRoutes,
 } from '@crawlee/browser';
 import { BrowserCrawler, Configuration, Router } from '@crawlee/browser';
@@ -22,7 +21,7 @@ export interface PuppeteerCrawlingContext<UserData extends Dictionary = Dictiona
     extends BrowserCrawlingContext<PuppeteerCrawler, Page, HTTPResponse, PuppeteerController, UserData>,
         PuppeteerContextUtils {}
 export interface PuppeteerHook extends BrowserHook<PuppeteerCrawlingContext, PuppeteerGoToOptions> {}
-export interface PuppeteerRequestHandler extends BrowserRequestHandler<LoadedContext<PuppeteerCrawlingContext>> {}
+export interface PuppeteerRequestHandler extends BrowserRequestHandler<PuppeteerCrawlingContext> {}
 export type PuppeteerGoToOptions = Parameters<Page['goto']>[1];
 
 export interface PuppeteerCrawlerOptions
