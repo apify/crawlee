6b9a348c846196029de5453f04158c93a143f0d8
Martin Adámek <banan23@gmail.com>
2025-06-11 15:33:10 +0200

    docs: bump docusaurus to 3.8.1 (#3009)

--- website/src/plugins/docusaurus-plugin-segment/segment.js
+++ website/src/plugins/docusaurus-plugin-segment/segment.js
@@ -2,14 +2,14 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 export default ExecutionEnvironment.canUseDOM ? {
     onRouteUpdate({ location }) {
-      // Don't track page views on development
-      if (process.env.NODE_ENV === 'production' && window.analytics) {
-        window.analytics.page({
-          app: 'crawlee',
-          path: location.pathname,
-          url: location.href,
-          search: location.search,
-        });
-      }
+        // Don't track page views on development
+        if (process.env.NODE_ENV === 'production' && window.analytics) {
+            window.analytics.page({
+                app: 'crawlee',
+                path: location.pathname,
+                url: location.href,
+                search: location.search,
+            });
+        }
     },
 } : null;
