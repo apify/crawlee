7ed3167d7441682f7524986e4b7823d1b825deba
Jindřich Bär <jindrichbar@gmail.com>
2025-07-03 13:48:19 +0200

    chore(e2e): make `playwright-multi-run` more stable (#3042)
    If the `maxRequestsPerCrawl` is not overshot, the total number of
    results can be equal to `30`.

--- test/e2e/playwright-multi-run/test.mjs
+++ test/e2e/playwright-multi-run/test.mjs
@@ -10,5 +10,5 @@ await initialize(testActorDirname);
 const { datasetItems } = await runActor(testActorDirname, 16384);
 
 // we cant assert number of requests as the stats KVS is being wiped on each `run` call, unlike the dataset
-await expect(datasetItems.length > 30, `Number of dataset items (actual: ${datasetItems.length}, expected: > 30)`);
+await expect(datasetItems.length >= 30, `Number of dataset items (actual: ${datasetItems.length}, expected: >= 30)`);
 await expect(validateDataset(datasetItems, ['url', 'pageTitle']), 'Dataset items validation');
