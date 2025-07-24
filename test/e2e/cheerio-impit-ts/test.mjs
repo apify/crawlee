9dc225fdc825746f6b701cc02a6c822e346d083f
Jindřich Bär <jindrichbar@gmail.com>
2025-07-24 09:45:58 +0200

    chore: fix `cheerio-impit-ts`

--- test/e2e/cheerio-impit-ts/test.mjs
+++ test/e2e/cheerio-impit-ts/test.mjs
@@ -13,5 +13,5 @@ const result = datasetItems[0];
 expect(result.body.length > 1000, 'HTML response is not empty');
 expect(result.title.toLowerCase().includes('crawlee'), 'HTML title is correct');
 expect(/Gecko\/\d{8} Firefox\/\d{2}/.test(result.userAgent), 'Impit correctly spoofs Firefox');
-expect(result.uuidJsonResponse.uuid !== undefined, 'JSON response contains UUID');
-expect(JSON.parse(result.uuidTextResponse).uuid !== undefined, 'Text response contains UUID');
+expect(result.clientIpJsonResponse.clientIp !== undefined, 'JSON response contains client IP');
+expect(JSON.parse(result.clientIpTextResponse).clientIp !== undefined, 'Text response contains client IP');
