f55cfea867660cce8f493de4fae85e3f6974824c
katzino <katzino@gmail.com>
2025-10-15 11:54:24 +0200

    fix: missing `url` param on page view [internal]

--- website/src/plugins/docusaurus-plugin-segment/segment.js
+++ website/src/plugins/docusaurus-plugin-segment/segment.js
@@ -1,15 +1,18 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 export default ExecutionEnvironment.canUseDOM ? {
-    onRouteUpdate({ location }) {
-        // Don't track page views on development
-        if (process.env.NODE_ENV === 'production' && window.analytics) {
-            window.analytics.page({
-                app: 'crawlee',
-                path: location.pathname,
-                url: location.href,
-                search: location.search,
-            });
-        }
+    onRouteUpdate() {
+        // this forces deferred execution that ensures `window.location` is in sync
+        setTimeout(() => {
+            // Don't track page views on development
+            if (process.env.NODE_ENV === 'production' && window.analytics) {
+                window.analytics.page({
+                    app: 'crawlee',
+                    path: window.location.pathname,
+                    url: window.location.href,
+                    search: window.location.search,
+                });
+            }
+        }, 0);
     },
 } : null;
