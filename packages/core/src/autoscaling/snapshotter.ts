67616a37a41e426393eebd092c28a57b27cb3ddd
Richard Solár <solar.richard@gmail.com>
2026-02-24 09:40:02 +0100

    update snapshotter

--- packages/core/src/autoscaling/snapshotter.ts
+++ packages/core/src/autoscaling/snapshotter.ts
@@ -6,7 +6,6 @@ import { betterClearInterval, betterSetInterval } from '@apify/utilities';
 
 import { EventType } from '../events/event_manager.js';
 import type { CrawleeLogger } from '../log.js';
-import { log as defaultLog } from '../log.js';
 import { serviceLocator } from '../service_locator.js';
 import type { SystemInfo } from './system_status.js';
 
@@ -151,7 +150,7 @@ export class Snapshotter {
             maxBlockedMillis = 50,
             maxUsedMemoryRatio = 0.9,
             maxClientErrors = 3,
-            log = defaultLog,
+            log = serviceLocator.getLogger(),
         } = options;
 
         this.log = log.child({ prefix: 'Snapshotter' });
