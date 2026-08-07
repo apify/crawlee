5eb114b003e845f0b4c733406b93ac9b4cb06474
Jindřich Bär <jindrichbar@gmail.com>
2026-08-07 13:52:31 +0200

    feat(utils): split exports into public and /internal entry points

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,10 +1,8 @@
-export * from './internals/blocked.js';
-export * from './internals/cheerio.js';
-export * from './internals/extract-urls.js';
-export * from './internals/general.js';
+export { htmlToText } from './internals/cheerio.js';
+export { downloadListOfUrls, extractUrls } from './internals/extract-urls.js';
+export type { DownloadListOfUrlsOptions, ExtractUrlsOptions } from './internals/extract-urls.js';
+export { sleep, expandShadowRoots } from './internals/general.js';
 export * as social from './internals/social.js';
 export * from './internals/open_graph_parser.js';
 export * from './internals/robots.js';
 export * from './internals/sitemap.js';
-export * from './internals/iterables.js';
-export * from './internals/url.js';
---
+++ packages/utils/src/internal.ts
@@ -0,0 +1,7 @@
+export * from './internals/blocked.js';
+export type { CheerioRoot, CheerioAPI, Cheerio, Element } from './internals/cheerio.js';
+export { extractUrlsFromCheerio } from './internals/cheerio.js';
+export { tryAbsoluteURL } from './internals/extract-urls.js';
+export { URL_NO_COMMAS_REGEX, URL_WITH_COMMAS_REGEX } from './internals/general.js';
+export * from './internals/iterables.js';
+export * from './internals/url.js';
